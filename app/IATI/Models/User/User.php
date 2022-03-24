1cf4720a6b1b0e1177430d7eb00de66c5e6f1903
sriza <sriza054@gmail.com>
2022-03-24 10:20:40 +0545

    23-implement-landing-page-UI
    - [x] phpdocs on classes
    - [x] title on blade file changed to 'IATI publisher'

--- app/IATI/Models/User/User.php
+++ app/IATI/Models/User/User.php
@@ -9,6 +9,9 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
+/**
+ * Class User.
+ */
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
--- app/IATI/Services/User/UserService.php
+++ app/IATI/Services/User/UserService.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace App\IATI\Services\User;
 
+/**
+ * Class UserService.
+ */
 class UserService
 {
     /**
