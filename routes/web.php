d3c3bfeab792657a5c8c80d2794f0ebda70c67ee
Sanil Manandhar <sanilmanandhar@gmail.com>
2022-04-01 16:30:34 +0545

    Test line added

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//Test Line
 Route::get('/', [App\Http\Controllers\Web\WebController::class, 'index'])->name('web');
 
 Route::get('/activities', function () {
