7fe091f5b48c33976f9ea91d440cf8ae3dcef23b
sriza <sriza054@gmail.com>
2022-04-07 13:34:50 +0545

    31-user-module
    - [x] code refactor
    - [x] email verification
    - [x] progress bar

--- app/IATI/Repositories/User/UserRepository.php
+++ app/IATI/Repositories/User/UserRepository.php
@@ -2,11 +2,18 @@
 
 declare(strict_types=1);
 
-namespace App\IATI\Services\User;
+namespace App\IATI\Repositories\User;
+
+use App\IATI\Models\User\User;
+use App\IATI\Repositories\Repository;
 
 /**
  * Class UserRepository.
  */
-class UserRepository
+class UserRepository extends Repository
 {
+    public function getModel():string
+    {
+        return User::class;
+    }
 }
--- resources/views/web/register.blade.php
+++ resources/views/web/register.blade.php
@@ -1,6 +1,5 @@
 @extends('web.layouts.app')
 
 @section('content')
-  {{-- <register-form country={{json_encode($countries)}} registration_agency={{json_encode($registration_agencies)}}></register-form> --}}
-  <register-form></register-form>
+  <register-form :country='{{json_encode($countries)}}' :registration_agency='{{json_encode($registration_agencies)}}' ></register-form>
 @endsection
