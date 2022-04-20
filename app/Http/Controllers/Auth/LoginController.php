1b638ac523a05a670893ec8765d7cc4d738b6360
Manish Pradhan <manish.pradhan@yipl.com.np>
2022-04-20 14:54:35 +0545

    authentication test case added

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -7,6 +7,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 /**
  * Class LoginController.
@@ -48,8 +49,25 @@ class LoginController extends Controller
      *
      * @return string
      */
-    public function username()
+    public function username(): string
     {
         return 'username';
+    }
+
+    /**
+     * Validate the user login request.
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return void
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    protected function validateLogin(Request $request): void
+    {
+        $request->validate([
+      $this->username() => 'required|string',
+      'password'        => 'required|string',
+    ]);
     }
 }
