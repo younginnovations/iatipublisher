f324c0e5dfa91511c8c2fbad5ed7d5c3cbdb1bdf
Manoz shrestha <manozstha77@gmail.com>
2022-04-29 13:34:48 +0545

    54-setting-page-UI
    - [x] dynamic hover text width
    - [x] form space adjusted

--- resources/assets/js/components/HoverText.vue
+++ resources/assets/js/components/HoverText.vue
@@ -3,7 +3,7 @@
     <button class="">
       <svg-vue class="cursor-pointer text-base" icon="help"></svg-vue>
     </button>
-    <div class="help__text">
+    <div class="help__text" :class="hoverTextClass">
       <span class="font-bold">{{ props.name }}</span>
       <p v-html="props.hover_text"></p>
     </div>
@@ -11,16 +11,21 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
 
 export default defineComponent({
   props: {
     name: String,
     hover_text: String,
+    width: String,
   },
   setup(props) {
+    const hoverTextClass = ref('');
+    hoverTextClass.value = props.width ? props.width : 'w-60';
+
     return {
       props,
+      hoverTextClass,
     };
   },
 });
@@ -31,7 +36,7 @@ export default defineComponent({
   @apply relative;
 
   &__text {
-    @apply invisible absolute right-0 top-4 z-20 w-60 rounded bg-eggshell p-2 text-left text-xs text-n-40 opacity-0 duration-200;
+    @apply invisible absolute right-0 top-4 z-20  rounded bg-eggshell p-2 text-left text-xs text-n-40 opacity-0 duration-200;
     transition: all 0.3s ease-out;
   }
 }
