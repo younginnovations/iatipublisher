1f71ba7a94ea7c5d6ef9237796d7e09e97ed0ce4
Manoz shrestha <manozstha77@gmail.com>
2022-05-31 17:23:49 +0545

    144-organisation-data
    - [x] organisation data page UI
    - [x] blade file for organisation data
    - [x] added element/group.json
    - [x] added new svg images
    - [x] packages of radial progress bar
    - [x] key added for v-for
    - [x] class name changed from listing page to page height
    - [x] elements config
    - [x] OrganisationElement renamed into ElementsDetail
    - [x] removed min-height from div
    - [x] right svg image given for elements detail
    - [x] elements config
    - [x] used breadcrumb components
    - [x] added all elements details
    - [x] link active
    - [x] words breaks for elements title
    - [x] svg images
    - [x] breadcrumb links
    - [x] replaced progress bar with radial progress bar in activity detail page
    - [x] modal in organisation page buttons
    - [x] elements config
    - [x] new svg images
    - [x] radial progress bar fixed
    - [x] organisation UI as design
    - [x] dynamic class format change
    - [x] file name changed
    - [x] elements name changed
    - [x] defer in script
    - [x] added basic information tab
    - [x] spacing and text change
    - [x] document link data
    - [x] code refactor
    - [x] removed unused code

--- resources/assets/js/views/activity/ActivityDetail.vue
+++ resources/assets/js/views/activity/ActivityDetail.vue
@@ -239,7 +239,10 @@
                 position="right"
               />
             </div>
-            <ProgressBar :percent="progress" class="mb-3" />
+            <RadialProgressBar
+              class="mb-3 h-20 text-8xl"
+              :isPercent="true"
+            ></RadialProgressBar>
             <span>Fill core elements to get 100% score</span>
           </div>
           <div class="activities__card elements">
@@ -264,7 +267,7 @@
             </div>
             <div class="flex justify-between">
               <div class="flex items-center space-x-1">
-                <svg-vue icon="double-tick" />
+                <svg-vue icon="double-tick" class="text-spring-50"></svg-vue>
                 <span>Completed</span>
               </div>
               <HoverText
@@ -364,7 +367,7 @@ import { useToggle } from '@vueuse/core';
 // components
 import { Result } from './elements/Index';
 import HoverText from 'Components/HoverText.vue';
-import ProgressBar from 'Components/ProgressBar.vue';
+import RadialProgressBar from 'Components/RadialProgressBar.vue';
 import Modal from 'Components/PopupModal.vue';
 import BtnComponent from 'Components/ButtonComponent.vue';
 import Toast from 'Components/Toast.vue';
@@ -375,13 +378,13 @@ import ActivityElement from 'Activity/partials/ActivityElement.vue';
 export default defineComponent({
   components: {
     HoverText,
-    ProgressBar,
     Elements,
     ActivityElement,
     Result,
     Modal,
     BtnComponent,
     Toast,
+    RadialProgressBar,
   },
   props: {
     elements: {
