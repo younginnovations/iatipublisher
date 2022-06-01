b6124a6e06fea762b59080cdb151375bdcf861e5
Manoz shrestha <manozstha77@gmail.com>
2022-06-01 17:17:26 +0545

    168-UI-bug-fix
    - [x] hamburger bug fixed
    - [x] 152 Bug: UI issues
    
    152 Bug: UI isssue over mozilla
    
    - [x] issue 3 and 4 completed
    - [x] padding fixed
    - [x] 148 Bug UI issues fix
    - [x] 152 Bug: UI isssue #187 fixed
    - [x] versioning

--- resources/assets/js/scripts/script.ts
+++ resources/assets/js/scripts/script.ts
@@ -1,21 +1,19 @@
 import $ from 'jquery';
 import 'select2';
 
-$(function () {
-  $(window).resize(() => {
-    $('body').on('click', '#hamburger', () => {
-      $('#nav-list').toggleClass('nav-active');
-      $('#hamburger').toggleClass('active');
-      $('body').toggleClass('overflow-hidden');
-    });
+$(document).ready(() => {
+  $('body').on('click', '#hamburger', () => {
+    $('#nav-list').toggleClass('nav-active');
+    $('#hamburger').toggleClass('active');
+    $('body').toggleClass('overflow-hidden');
+  });
 
-    // close the navMenu by clicking outside
-    $('body').on('click', 'window', (e) => {
-      if (e.target.id !== 'nav-list' && e.target.id !== 'hamburger') {
-        $('#nav-list').removeClass('nav-active');
-        $('#hamburger').removeClass('active');
-        $('body').removeClass('overflow-hidden');
-      }
-    });
+  // close the navMenu by clicking outside
+  $('body').on('click', (e) => {
+    if (e.target.id !== 'nav-list' && e.target.id !== 'hamburger') {
+      $('#nav-list').removeClass('nav-active');
+      $('#hamburger').removeClass('active');
+      $('body').removeClass('overflow-hidden');
+    }
   });
 });
