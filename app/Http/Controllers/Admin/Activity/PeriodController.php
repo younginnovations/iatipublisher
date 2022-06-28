ff5bb36707ed8e0e12bf40af784d942143e5608a
Sanil Manandhar <sanilmanandhar@gmail.com>
2022-06-28 17:01:07 +0545

    59-activity-complex-forms * [x] Migrations for indicator, period * [x] Models for indicator, period * [x] Updated elementJsonSchema.json file

---
+++ app/Http/Controllers/Admin/Activity/PeriodController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Activity;
+
+use App\Http\Controllers\Controller;
+use App\IATI\Models\Activity\Period;
+use Illuminate\Http\Request;
+
+class PeriodController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\IATI\Models\Activity\Period  $period
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Period $period)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\IATI\Models\Activity\Period  $period
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Period $period)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\IATI\Models\Activity\Period  $period
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Period $period)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\IATI\Models\Activity\Period  $period
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Period $period)
+    {
+        //
+    }
+}
