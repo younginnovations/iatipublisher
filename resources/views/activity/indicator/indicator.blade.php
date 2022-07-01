c1627426156bce2bd2614c73ac0783362f575348
Sanil Manandhar <sanilmanandhar@gmail.com>
2022-07-01 10:12:41 +0545

    59-activity-complex-forms * [x] Forms for indicator and period * [x] Save data to database and edit forms

---
+++ resources/views/activity/indicator/indicator.blade.php
@@ -0,0 +1,65 @@
+@extends('admin.layouts.app')
+
+@section('content')
+    <section class="section min-h-[calc(100vh_-_60px)]">
+
+        <div class="px-10 pt-4 pb-[71px]">
+            @include('admin.layouts.activityTitle')
+            <div class="activities">
+                <aside class="activities__sidebar">
+                    <elements-note></elements-note>
+                </aside>
+                <div class="activities__content">
+                    <div class="py-[6.06%] px-[12%] bg-white">
+                        <div class="status flex justify-end rounded-lg mb-1.5">
+                            <div class="flex status text-xs leading-relaxed text-salmon-50">
+                                <b class="mr-2 text-base leading-3">.</b><span>not completed</span>
+                            </div>
+                        </div>
+                        <div class="title flex items-center mb-4">
+                            <div class="text-sm shrink-0 uppercase text-n-40 font-bold">Result Indicator</div>
+                            <div class="line grow h-px border-b border-n-40 ml-4"></div>
+                        </div>
+
+                        {!! form($form) !!}
+{{--                        <div class="hidden collection-container title" form_type="title_narrative"--}}
+{{--                             data-prototype="{{ form_row($form->title->getChildren()[0]->getChild('narrative')->prototype()) }}">--}}
+{{--                        </div>--}}
+{{--                        <div class="hidden collection-container title" form_type="description_narrative"--}}
+{{--                             data-prototype="{{ form_row($form->description->getChildren()[0]->getChild('narrative')->prototype()) }}">--}}
+{{--                        </div>--}}
+{{--                        <div class="hidden parent-collection" form_type="document_link"--}}
+{{--                             data-prototype="{{ form_row($form->document_link->prototype()) }}">--}}
+{{--                        </div>--}}
+{{--                        <div class="hidden collection-container title" form_type="category"--}}
+{{--                             data-prototype="{{ form_row($form->document_link->getChildren()[0]->getChild('category')->prototype()) }}">--}}
+{{--                        </div>--}}
+{{--                        <div class="hidden collection-container title" form_type="language"--}}
+{{--                             data-prototype="{{ form_row($form->document_link->getChildren()[0]->getChild('language')->prototype()) }}">--}}
+{{--                        </div>--}}
+{{--                        <div class="hidden parent-collection" form_type="reference"--}}
+{{--                             data-prototype="{{ form_row($form->reference->prototype()) }}">--}}
+{{--                        </div>--}}
+                        <div class="hidden collection-container title" form_type="comment_narrative"
+                             data-prototype="{{ form_row($form->baseline->getChildren()[0]->getChild('comment')->getChildren()[0]->getChild('narrative')->prototype()) }}">
+                        </div>
+                        <div class="hidden collection-container title" form_type="dimension"
+                             data-prototype="{{ form_row($form->baseline->getChildren()[0]->getChild('dimension')->prototype()) }}">
+                        </div>
+{{--                        <div class="hidden collection-container title" form_type="document_link_category"--}}
+{{--                             data-prototype="{{ form_row($form->document_link->getChildren()[0]->getChild('category')->prototype()) }}">--}}
+{{--                        </div>--}}
+
+
+                        <div class="hidden collection-container title" form_type="title_narrative"
+                             data-prototype="{{ form_row($form->baseline->getChildren()[0]->getChild('document_link')->getChildren()[0]->getChild('title')->getChildren()[0]->getChild('narrative')->prototype()) }}">
+                        </div>
+                        <div class="hidden collection-container title" form_type="document_link_category"
+                             data-prototype="{{ form_row($form->baseline->getChildren()[0]->getChild('document_link')->getChildren()[0]->getChild('category')->prototype()) }}">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
