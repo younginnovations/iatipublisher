75dcb24ba3ec34a39f18f9a310371397f9b4d0ae
Sanil Manandhar <sanilmanandhar@gmail.com>
2022-07-01 15:06:27 +0545

    59-activity-complex-forms * [x] Updated validation for result * [x] Added validations for indicator and period

--- app/Http/Requests/Activity/Indicator/IndicatorRequest.php
+++ app/Http/Requests/Activity/Indicator/IndicatorRequest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Http\Requests\Activity\Indicator;
 
 use App\Http\Requests\Activity\ActivityBaseRequest;
+use Illuminate\Support\Arr;
 
 /**
  * Class IndicatorRequest.
@@ -42,6 +43,15 @@ class IndicatorRequest extends ActivityBaseRequest
     {
         $rules = [];
 
+        $rules = array_merge(
+            $rules,
+            $this->getRulesForNarrative($formFields['title'], 'title.0'),
+            $this->getRulesForNarrative($formFields['description'], 'description.0'),
+            $this->getRulesForDocumentLink($formFields['document_link']),
+            $this->getRulesForReference($formFields['reference']),
+            $this->getRulesForBaseline($formFields['baseline']),
+        );
+
         return $rules;
     }
 
@@ -55,6 +65,123 @@ class IndicatorRequest extends ActivityBaseRequest
     protected function getMessagesForIndicator(array $formFields): array
     {
         $messages = [];
+
+        $messages = array_merge(
+            $messages,
+            $this->getMessagesForNarrative($formFields['title'], 'title.0'),
+            $this->getMessagesForNarrative($formFields['description'], 'description.0'),
+            $this->getMessagesForDocumentLink($formFields['document_link']),
+            $this->getMessagesForReference($formFields['reference']),
+            $this->getMessagesForBaseline($formFields['baseline']),
+        );
+
+        return $messages;
+    }
+
+    /**
+     * returns rules for reference.
+     *
+     * @param $formFields
+     *
+     * @return array
+     */
+    protected function getRulesForReference($formFields): array
+    {
+        $rules = [];
+
+        foreach ($formFields as $referenceIndex => $reference) {
+            $referenceForm = sprintf('reference.%s', $referenceIndex);
+            $rules[sprintf('%s.indicator_uri', $referenceForm)] = 'nullable|url';
+        }
+
+        return $rules;
+    }
+
+    /**
+     * returns messages for reference.
+     *
+     * @param $formFields
+     *
+     * @return array
+     */
+    protected function getMessagesForReference($formFields): array
+    {
+        $messages = [];
+
+        foreach ($formFields as $referenceIndex => $reference) {
+            $referenceForm = sprintf('reference.%s', $referenceIndex);
+            $messages[sprintf('%s.indicator_uri.url', $referenceForm)]
+                = 'The @indicator-uri field must be a valid url.';
+        }
+
+        return $messages;
+    }
+
+    /**
+     * returns rules for baseline.
+     *
+     * @param $formFields
+     *
+     * @return array
+     */
+    protected function getRulesForBaseline($formFields): array
+    {
+        $rules = [];
+
+        foreach ($formFields as $baselineIndex => $baseline) {
+            $baselineForm = sprintf('baseline.%s', $baselineIndex);
+
+            $rules[sprintf('%s.year', $baselineForm)] = 'nullable|date_format:Y|digits:4';
+
+            if ((request()->get('measure') == 2) &&
+                (Arr::get($baseline, 'value', null))) {
+                $rules[sprintf('%s.value', $baselineForm)] = 'nullable|numeric|gte:0';
+            } elseif ((request()->get('measure') == 1) &&
+                (Arr::get($baseline, 'value', null))) {
+                $rules[sprintf('%s.value', $baselineForm)] = 'nullable|numeric';
+            }
+
+            $rules = array_merge(
+                $rules,
+                $this->getRulesForNarrative(
+                    $baseline['comment'][0]['narrative'],
+                    sprintf('%s.comment.0', $baselineForm)
+                ),
+                $this->getRulesForDocumentLink($baseline['document_link'], $baselineForm),
+            );
+        }
+
+        return $rules;
+    }
+
+    /**
+     * returns messages for baseline.
+     *
+     * @param $formFields
+     *
+     * @return array
+     */
+    protected function getMessagesForBaseline($formFields): array
+    {
+        $messages = [];
+
+        foreach ($formFields as $baselineIndex => $baseline) {
+            $baselineForm = sprintf('baseline.%s', $baselineIndex);
+            $messages[sprintf('%s.year.date_format', $baselineForm)] = 'The @year field is not valid.';
+            $messages[sprintf('%s.year.digits', $baselineForm)] = 'The @year field must have 4 digits.';
+
+            $messages[sprintf('%s.value.numeric', $baselineForm)] = 'The @value field must be a number.';
+            $messages[sprintf('%s.value.gte', $baselineForm)] = 'The @value field must be greater or equal to 0.';
+
+            $messages = array_merge(
+                $messages,
+                $this->getMessagesForNarrative(
+                    $baseline['comment'][0]['narrative'],
+                    sprintf('%s.comment.0', $baselineForm)
+                ),
+                $this->getMessagesForDocumentLink($baseline['document_link'], $baselineForm),
+            );
+        }
 
         return $messages;
     }
--- app/Http/Requests/Activity/Period/PeriodRequest.php
+++ app/Http/Requests/Activity/Period/PeriodRequest.php
@@ -42,6 +42,14 @@ class PeriodRequest extends ActivityBaseRequest
     {
         $rules = [];
 
+        $rules = array_merge(
+            $rules,
+            $this->getRulesForResultPeriod($formFields['period_start'], 'period_start'),
+            $this->getRulesForResultPeriod($formFields['period_end'], 'period_end'),
+            $this->getRulesForTarget($formFields['target'], 'target'),
+            $this->getRulesForTarget($formFields['actual'], 'actual')
+        );
+
         return $rules;
     }
 
@@ -55,6 +63,102 @@ class PeriodRequest extends ActivityBaseRequest
     protected function getMessagesForPeriod(array $formFields): array
     {
         $messages = [];
+
+        $messages = array_merge(
+            $messages,
+            $this->getMessagesForResultPeriod($formFields['period_start'], 'period_start'),
+            $this->getMessagesForResultPeriod($formFields['period_end'], 'period_end'),
+            $this->getMessagesForTarget($formFields['target'], 'target'),
+            $this->getMessagesForTarget($formFields['actual'], 'actual')
+        );
+
+        return $messages;
+    }
+
+    /**
+     * returns rules for period start and period end.
+     *
+     * @param $formFields
+     * @param $periodType
+     *
+     * @return array
+     */
+    protected function getRulesForResultPeriod($formFields, $periodType): array
+    {
+        $rules = [];
+
+        foreach ($formFields as $periodStartKey => $periodStartVal) {
+            $rules[sprintf('%s.%s.date', $periodType, $periodStartKey)] = 'nullable|date';
+        }
+
+        return $rules;
+    }
+
+    /**
+     * returns messages for period start and period end.
+     *
+     * @param $formFields
+     * @param $periodType
+     *
+     * @return array
+     */
+    protected function getMessagesForResultPeriod($formFields, $periodType): array
+    {
+        $messages = [];
+
+        foreach ($formFields as $periodStartKey => $periodStartVal) {
+            $messages[sprintf('%s.%s.date.date', $periodType, $periodStartKey)] = 'The @date field must be a proper date.';
+        }
+
+        return $messages;
+    }
+
+    /**
+     * returns rules for Target.
+     *
+     * @param $formFields
+     * @param $valueType
+     *
+     * @return array
+     */
+    protected function getRulesForTarget($formFields, $valueType): array
+    {
+        $rules = [];
+
+        foreach ($formFields as $targetIndex => $target) {
+            $targetForm = sprintf('%s.%s', $valueType, $targetIndex);
+
+            $rules = array_merge(
+                $rules,
+                $this->getRulesForNarrative($target['comment'][0]['narrative'], sprintf('%s.comment.0', $targetForm)),
+                $this->getRulesForDocumentLink($target['document_link'], $targetForm),
+            );
+        }
+
+        return $rules;
+    }
+
+    /**
+     * returns messages for target.
+     *
+     * @param $formFields
+     * @param $valueType
+     *
+     * @return array
+     */
+    protected function getMessagesForTarget($formFields, $valueType): array
+    {
+        $messages = [];
+
+        foreach ($formFields as $targetIndex => $target) {
+            $targetForm = sprintf('%s.%s', $valueType, $targetIndex);
+
+            $messages = array_merge(
+                $messages,
+                $this->getMessagesForNarrative($target['comment'][0]['narrative'], sprintf('%s.comment.0', $targetForm)),
+                $this->getMessagesForDocumentLink($target['document_link'], $targetForm),
+            );
+        }
 
         return $messages;
     }
