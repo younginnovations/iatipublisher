4df7cd898fff01c83dfb4c0ac0eda24416f3eb10
sriza <sriza054@gmail.com>
2022-07-08 13:22:59 +0545

    - [x] hover text - [x] location fixes - [x] transaction and indicator add more fixes

--- resources/assets/js/scripts/formbuilder.ts
+++ resources/assets/js/scripts/formbuilder.ts
@@ -7,7 +7,6 @@ class FormBuilder {
     ev.preventDefault();
     let target = ev.target as EventTarget;
     let container = $(target).attr('form_type') ? $(`.collection-container[form_type ='${$(target).attr('form_type')}']`) : $('.collection-container');
-    console.log(container);
 
     let count = $(target).attr('child_count')
       ? parseInt($(target).attr('child_count') as string) + 1
@@ -17,21 +16,27 @@ class FormBuilder {
       ? parseInt($(target).attr('parent_count') as string)
       : $(target).parent().prevAll('.multi-form').length;
 
-    let wrapper_parent_count = $(target).attr('wrapper_parent_count') ?
-      $(target).attr('wrapper_parent_count') :
-      $(target).parent().find('.wrapper-child-body').length;
-
-    let isParent = $(target).attr('has_children');
+    let wrapper_parent_count = $(target).attr('wrapped_parent_count') ?
+      parseInt($(target).attr('wrapped_parent_count') as string) :
+      $(target).parent('.subelement').find('.wrapped-child-body').length;
 
     let proto = container
       .data('prototype')
       .replace(/__PARENT_NAME__/g, parent_count);
     proto = proto.replace(/__NAME__/g, count);
-    proto = proto.replace(/__WRAPPER_NAME__/g, wrapper_parent_count??0);
+    proto = proto.replace(/__WRAPPER_NAME__/g, wrapper_parent_count);
 
     $(target).prev().append($(proto));
+    if ($(target).attr('has_child_collection')) {
+      let child =  $(target).prev('.subelement').children('.wrapped-child-body').last();
+      console.log(child);
+      $(target).prev('.subelement').children('.wrapped-child-body').last().find('.add_to_collection').attr('wrapped_parent_count', count );
+      $(target).prev('.subelement').children('.wrapped-child-body').last().find('.add_to_collection').attr('parent_count', parent_count );
+    }
 
-    $(target).prev().find('.wrapped-child-body').last().find('.add_to_collection').attr('wrapper_parent_count', wrapper_parent_count??0);
+
+
+    $(target).prev().find('.wrapped-child-body').last().find('.add_to_collection').attr('wrapper_parent_count', wrapper_parent_count ?? 0);
 
     if ($(target).attr('form_type')) {
       $(target).prev().last().find('.select2').select2({
@@ -45,7 +50,7 @@ class FormBuilder {
           )
         );
 
-      $(target).prev('.subelement').find('.wrapped-child-body').last().find('.sub-attribute')
+      $(target).prev('.subelement').children('.wrapped-child-body').last().find('.sub-attribute')
         .wrapAll(
           $(
             '<div class="form-field-group flex flex-wrap rounded-br-lg border-y border-r border-spring-50 sub-attribute-wrapper mt-6"></div>'
@@ -66,13 +71,11 @@ class FormBuilder {
     $(target).attr('child_count', count);
     this.aidTypeVocabularyHideField();
     this.sectorVocabularyHideField();
-    this.transactionAidTypeVocabularyHideField();
   }
 
   // adds parent collection
   public addParentForm(ev: Event): void {
     ev.preventDefault();
-    console.log('parent');
     let target = ev.target as EventTarget;
     let container = $(target).attr('form_type') ? $(`.parent-collection[form_type ='${$(target).attr('form_type')}']`) : $('.parent-collection');
 
@@ -87,8 +90,6 @@ class FormBuilder {
       placeholder: 'Select an option',
     });
     $(target).prev().find('.multi-form').last().find('.add_to_collection').attr('parent_count', count);
-    console.log($(target).prev().find('.multi-form').length);
-
 
     this.addWrapperOnAdd(target);
 
@@ -100,6 +101,7 @@ class FormBuilder {
     this.recipientVocabularyHideField();
     this.policyVocabularyHideField();
     this.tagVocabularyHideField();
+    this.transactionAidTypeVocabularyHideField();
   }
 
   // deletes collection
@@ -141,7 +143,7 @@ class FormBuilder {
       $(this).find('.attribute')
         .wrapAll(
           $(
-            '<div class="form-field-group flex flex-wrap rounded-br-lg border-y border-r border-spring-50 attribute-wrapper"></div>'
+            '<div class="form-field-group flex flex-wrap rounded-br-lg border-y border-r border-spring-50 attribute-wrapper mb-4"></div>'
           )
         );
     })
@@ -150,7 +152,7 @@ class FormBuilder {
       $(this).find('.sub-attribute')
         .wrapAll(
           $(
-            '<div class="form-field-group flex flex-wrap rounded-br-lg border-y border-r border-spring-50 sub-attribute-wrapper"></div>'
+            '<div class="form-field-group flex flex-wrap rounded-br-lg border-y border-r border-spring-50 sub-attribute-wrapper mb-4"></div>'
           )
         );
     });
@@ -161,7 +163,7 @@ class FormBuilder {
       .find('.attribute')
       .wrapAll(
         $(
-          '<div class="form-field-group flex flex-wrap rounded-br-lg border-y border-r border-spring-50 attribute-wrapper"></div>'
+          '<div class="form-field-group flex flex-wrap rounded-br-lg border-y border-r border-spring-50 attribute-wrapper mb-4"></div>'
         )
       );
 
@@ -169,7 +171,7 @@ class FormBuilder {
       $(this).find('.sub-attribute')
         .wrapAll(
           $(
-            '<div class="form-field-group flex flex-wrap rounded-br-lg border-y border-r border-spring-50 sub-attribute-wrapper"></div>'
+            '<div class="form-field-group flex flex-wrap rounded-br-lg border-y border-r border-spring-50 sub-attribute-wrapper mb-4"></div>'
           )
         );
     });
@@ -447,7 +449,7 @@ class FormBuilder {
    * Hide Transaction Aid Type Select Fields
    */
   public hideTransactionAidTypeSelectField(index: JQuery, value: string) {
-    let aid_type = 'select[id*="[aid_type]"]',
+    let aid_type = 'select[id*="[aid_type_code]"]',
       earmarking_category = 'select[id*="[earmarking_category]"]',
       earmarking_modality = 'select[id*="[earmarking_modality]"]',
       cash_and_voucher_modalities =
@@ -455,11 +457,11 @@ class FormBuilder {
       case1 =
         'select[id*="[earmarking_category]"],select[id*="[earmarking_modality]"],select[id*="[cash_and_voucher_modalities]"]',
       case2 =
-        'select[id*="[aid_type]"],select[id*="[earmarking_modality]"],select[id*="[cash_and_voucher_modalities]"]',
+        'select[id*="[aid_type_code]"],select[id*="[earmarking_modality]"],select[id*="[cash_and_voucher_modalities]"]',
       case3 =
-        'select[id*="[aid_type]"],select[id*="[earmarking_category]"],select[id*="[cash_and_voucher_modalities]"]',
+        'select[id*="[aid_type_code]"],select[id*="[earmarking_category]"],select[id*="[cash_and_voucher_modalities]"]',
       case4 =
-        'select[id*="[aid_type]"],select[id*="[earmarking_category]"],select[id*="[earmarking_modality]"]';
+        'select[id*="[aid_type_code]"],select[id*="[earmarking_category]"],select[id*="[earmarking_modality]"]';
 
     switch (value) {
       case '2':
@@ -553,7 +555,7 @@ class FormBuilder {
       policymaker_vocabulary.on('select2:clear', (e) => {
         const target = e.target as HTMLElement;
 
-        this.hidePolicyMakerField($(target), '');
+        this.hidePolicyMakerField($(target), '1');
       });
     }
   }
@@ -567,8 +569,6 @@ class FormBuilder {
         'input[id*="[policy_marker_text]"],input[id*="[vocabulary_uri]"]',
       case1 = 'input[id*="[policy_marker_text]"],input[id*="[vocabulary_uri]"]',
       case2 = 'select[id*="[policy_marker]"]';
-
-    console.log('here');
 
     switch (value) {
       case '1':
@@ -606,6 +606,12 @@ class FormBuilder {
       default:
         index
           .closest('.form-field-group')
+          .find(case1_show)
+          .show()
+          .closest('.form-field')
+          .show();
+        index
+          .closest('.form-field-group')
           .find(case1)
           .val('')
           .trigger('change')
@@ -639,7 +645,7 @@ class FormBuilder {
       sector_vocabulary.on('select2:clear', (e) => {
         const target = e.target as HTMLElement;
 
-        this.hideSectorField($(target), '');
+        this.hideSectorField($(target), '1');
       });
     }
   }
@@ -807,7 +813,7 @@ class FormBuilder {
       region_vocabulary.on('select2:clear', (e) => {
         const target = e.target as HTMLElement;
 
-        this.hideRecipientRegionField($(target), '');
+        this.hideRecipientRegionField($(target), '1');
       });
     }
   }
@@ -929,7 +935,7 @@ class FormBuilder {
       tag_vocabulary.on('select2:clear', (e) => {
         const target = e.target as HTMLElement;
 
-        this.hideTagField($(target), '');
+        this.hideTagField($(target), '1');
       });
     }
   }
@@ -1040,10 +1046,9 @@ $(function () {
   formBuilder.addWrapper();
   formBuilder.hideShowFormFields();
   formBuilder.updateActivityIdentifier();
-  console.log('here');
 
   $('.delete').on('click', () => {
-    console.log('clicked');
+    // console.log('clicked');
   })
 
   $('body').on('click', '.add_to_collection', (event: Event) => {
@@ -1106,13 +1111,11 @@ $(function () {
   $('body').on('change', 'input[id*="document"]', function () {
     let endpoint = $('.endpoint').attr('endpoint') ?? '';
     let file_name = ($(this).val() ?? '').toString();
-    console.log(`${endpoint}/${(file_name?.split('\\').pop())?.replace(' ', '_')}`);
     $(this).closest('.form-field-group').find('input[id*="[url]"]').val(`${endpoint}/${(file_name?.split('\\').pop())?.replace(' ', '_')}`);
   })
 
-  $('body').on('select2:open','.select2', (e)=> {
+  $('body').on('select2:open', '.select2', (e) => {
     let target = e.target;
-    console.log($('.select2-search__field'));
     $('.select2-search__field').trigger("focus");
   })
 
