e55683f8682e1504f50692c8613510b352963eb5
Manish Pradhan <manish.pradhan@yipl.com.np>
2022-07-21 16:28:16 +0545

    sector test added

---
+++ tests/Feature/Element/SectorCompleteTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature\Element;
+
+class SectorCompleteTest extends ElementCompleteTest
+{
+    private string $element = 'sector';
+
+    public function __construct(?string $name = null, array $data = [], $dataName = '')
+    {
+        parent::__construct($name, $data, $dataName);
+
+        $this->activityObj->element = $this->element;
+    }
+
+    public function test_sector_type_mandatory_attributes()
+    {
+        $this->test_mandatory_attributes($this->element, ['code', 'text', 'category_code', 'sdg_goal', 'sdg_target']);
+    }
+
+    public function test_sector_type_mandatory_sub_elements()
+    {
+        $this->test_mandatory_sub_elements($this->element, ['narrative'=>['language']]);
+    }
+
+    public function test_sector_type_element_complete()
+    {
+        $sector_typeData = json_decode('[{"sector_vocabulary":"1","code":"11110","percentage":"100","narrative":[{"narrative":"sector-1-narrative1","language":"aa"}]},{"sector_vocabulary":"2","category_code":"111","percentage":"100","narrative":[{"narrative":"sector-2-narrative1","language":"ab"}]},{"sector_vocabulary":"3","text":"vocab-3","percentage":"100","narrative":[{"narrative":"sector-3-narrative1","language":"am"}]},{"sector_vocabulary":"4","text":"vocab-4","percentage":"100","narrative":[{"narrative":"sector-4-narrative1","language":"ba"}]},{"sector_vocabulary":"5","text":"vocab-5","percentage":"100","narrative":[{"narrative":"sector-5-narrative1","language":"bn"}]},{"sector_vocabulary":"6","text":"vocab-6","percentage":"100","narrative":[{"narrative":null,"language":"en"}]},{"sector_vocabulary":"7","sdg_goal":"1","percentage":"100","narrative":[{"narrative":null,"language":"fr"}]},{"sector_vocabulary":"8","sdg_target":"1.1","percentage":"100","narrative":[{"narrative":null,"language":"gr"}]},{"sector_vocabulary":"9","text":"vocab-9","percentage":"100","narrative":[{"narrative":null,"language":"sp"}]},{"sector_vocabulary":"10","text":"vocab-10","percentage":"100","narrative":[{"narrative":null,"language":"hr"}]},{"sector_vocabulary":"99","vocabulary_uri":"https:\/\/www.google.com","text":"vocab-99","percentage":"100","narrative":[{"narrative":null,"language":"aa"}]}]', true);
+
+        $this->test_level_one_multi_dimensional_element_complete($this->element, $sector_typeData);
+    }
+}
