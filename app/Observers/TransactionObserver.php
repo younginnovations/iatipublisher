cc9a64a0641d17c2c3eaef8a4a149781e155fe8e
Manish Pradhan <manish.pradhan@yipl.com.np>
2022-08-04 13:18:02 +0545

    transaction observer finished

--- app/Observers/TransactionObserver.php
+++ app/Observers/TransactionObserver.php
@@ -3,23 +3,53 @@
 namespace App\Observers;
 
 use App\IATI\Models\Activity\Transaction;
+use App\IATI\Services\ElementCompleteService;
 
 class TransactionObserver
 {
-    public function updateActivityElementStatus($transaction)
+    protected ElementCompleteService $elementCompleteService;
+
+    /**
+     * Activity observer constructor.
+     */
+    public function __construct()
     {
-        $elementStatus = $transaction->activity->element_status;
-        $elementStatus['transactions'] = $transaction->activity->getTransactionsElementCompletedAttribute();
-        $transaction->activity->element_status = $elementStatus;
-        $transaction->activity->save();
+        $this->elementCompleteService = new ElementCompleteService();
     }
 
-    public function created(Transaction $transaction)
+    /**
+     * @param $transaction
+     *
+     * @return void
+     * @throws \JsonException
+     */
+    public function updateActivityElementStatus($transaction): void
+    {
+        $elementStatus = $transaction->activity->element_status;
+        $elementStatus['transactions'] = $this->elementCompleteService->isTransactionsElementCompleted($transaction->activity);
+
+        $transaction->activity->element_status = $elementStatus;
+        $transaction->activity->saveQuietly();
+    }
+
+    /**
+     * @param Transaction $transaction
+     *
+     * @return void
+     * @throws \JsonException
+     */
+    public function created(Transaction $transaction): void
     {
         $this->updateActivityElementStatus($transaction);
     }
 
-    public function updated(Transaction $transaction)
+    /**
+     * @param Transaction $transaction
+     *
+     * @return void
+     * @throws \JsonException
+     */
+    public function updated(Transaction $transaction): void
     {
         $this->updateActivityElementStatus($transaction);
     }
