a2c698b8361c7f754a16674f57030e0483899ce8
sriza <sriza054@gmail.com>
2022-08-26 14:13:07 +0545

    period and indicator html and element_criteria not found issue fixed

--- resources/views/vendor/laravel-form-builder/collection.php
+++ resources/views/vendor/laravel-form-builder/collection.php
@@ -8,10 +8,10 @@
         <?php endif; ?>
 
         <?php if ($showLabel && $options['label'] !== false && $options['label_show'] && strtolower($options['label']) !== "narrative") : ?>
-            <?php if ($options['options']['element_criteria'] === 'mandatory') : ?>
-                <?= HTML::decode(Form::customLabel($name, '<svg-vue icon="star" class="mr-2"></svg-vue>' . $options['label'], $options['label_attr'])) ?>
-            <?php elseif ($options['options']['element_criteria'] === 'recommended') : ?>
-                <?= HTML::decode(Form::customLabel($name, '<svg-vue icon="moon" class="mr-2"></svg-vue>' . $options['label'], $options['label_attr'])) ?>
+            <?php if (isset($options['options']['element_criteria']) && $options['options']['element_criteria'] === 'mandatory') : ?>
+                <?= htmlspecialchars_decode(Form::customLabel($name, '<svg-vue icon="star" class="mr-2"></svg-vue>' . $options['label'], $options['label_attr'])) ?>
+            <?php elseif (isset($options['options']['element_criteria']) && $options['options']['element_criteria'] === 'recommended') : ?>
+                <?= htmlspecialchars_decode(Form::customLabel($name, '<svg-vue icon="moon" class="mr-2"></svg-vue>' . $options['label'], $options['label_attr'])) ?>
             <?php else : ?>
                 <?= Form::customLabel($name,$options['label'], $options['label_attr']) ?>
             <?php endif; ?>
