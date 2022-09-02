88b62e5bfcc5c416a3367963de710734405e5e48
Manish Pradhan <manish.pradhan@yipl.com.np>
2022-09-02 15:00:43 +0545

    core element shown for collaboration_type

--- resources/views/admin/activity/show.blade.php
+++ resources/views/admin/activity/show.blade.php
@@ -1,10 +1,18 @@
 @extends('admin.layouts.app')
 @section('content')
-    <activities-detail :elements="{{ json_encode($elements) }}" :groups="{{ json_encode($elementGroups) }}"
-        :activity="{{ json_encode($activity) }}" :progress="{{ $progress }}" :lang="{{ json_encode('lang file') }}"
-        :types="{{ json_encode($types) }}" :status="{{ json_encode($status) }}" :toast="{{ json_encode($toast) }}"
-        :results="{{ json_encode($results) }}" :has_indicator="{{ $hasIndicatorPeriod['indicator'] }}"
-        :has_period="{{ $hasIndicatorPeriod['period'] }}" :transactions="{{ json_encode($transactions) }}"
+    <activities-detail
+        :elements="{{ json_encode($elements) }}"
+        :groups="{{ json_encode($elementGroups) }}"
+        :activity="{{ json_encode($activity) }}"
+        :progress="{{ $progress }}"
+        :lang="{{ json_encode('lang file') }}"
+        :types="{{ json_encode($types) }}"
+        :status="{{ json_encode($status) }}"
+        :toast="{{ json_encode($toast) }}"
+        :results="{{ json_encode($results) }}"
+        :has_indicator="{{ $hasIndicatorPeriod['indicator'] }}"
+        :has_period="{{ $hasIndicatorPeriod['period'] }}"
+        :transactions="{{ json_encode($transactions) }}"
         :core-completed="{{ json_encode($coreCompleted) }}"
         :iati-validator-response="{{ json_encode($iatiValidatorResponse) }}">
     </activities-detail>
