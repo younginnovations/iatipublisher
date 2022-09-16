a4aa61b1375720fa12d134c18d51b1b0f58d5f65
Manish Pradhan <manish.pradhan@yipl.com.np>
2022-09-16 18:28:44 +0545

    codes managed9

--- app/Imports/CsvToArray.php
+++ app/Imports/CsvToArray.php
@@ -1,12 +1,19 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Imports;
 
 use Maatwebsite\Excel\Concerns\ToArray;
 
 class CsvToArray implements ToArray
 {
-    public function array(array $rows)
+    /**
+     * @param array $rows
+     *
+     * @return array
+     */
+    public function array(array $rows): array
     {
         return $rows;
     }
