6652ed4b913a4607bee628472dc94edbb51cfd75
Manoz shrestha <manozstha77@gmail.com>
2022-09-23 16:08:19 +0545

    523-sign-up-page-responsive
    - [x] Register page responsiveness
    - [x] removed stylelint-order config

--- resources/assets/js/views/web/EmailVerification.vue
+++ resources/assets/js/views/web/EmailVerification.vue
@@ -1,15 +1,17 @@
 <template>
   <form method="POST" action="" class="form">
     <div class="grid justify-items-center gap-6 tracking-normal xl:px-2">
-      <span class="text-2xl font-bold text-n-50"
-        >Please verify your account</span
-      >
-      <p class="text-base text-n-40">
-        A verification email has been sent to
-        <span class="font-bold text-n-50"> {{ props.email }}</span
-        >. Please click on the link in the email to verify and activate your new
-        IATI publisher account.
-      </p>
+      <div class="space-y-4 sm:space-y-6">
+        <p class="text-center text-xl font-bold text-n-50 sm:text-2xl">
+          Please verify your account
+        </p>
+        <p class="text-sm text-n-40 sm:text-base">
+          A verification email has been sent to
+          <span class="font-bold text-n-50"> {{ props.email }}</span
+          >. Please click on the link in the email to verify and activate your
+          new IATI publisher account.
+        </p>
+      </div>
       <svg-vue class="verification" icon="ic-verification" />
       <a href="/activities" class="btn btn-next get-started-btn">
         Get started
