9636c4b629aad4b2ddf75ad0e24f1525a6f69f18
Bikram Tuladhar <bikramtuladhar2011@gmail.com>
2022-09-29 13:41:14 +0545

    fixes bash script

--- packages/development/seal-secret.sh
+++ packages/development/seal-secret.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-cat secret.yaml | kubeseal --controller-namespace kube-system \
+kubeseal --controller-namespace kube-system \
     --controller-name sealed-secrets-sealed-secrets-controller \
     --format yaml \
-    > sealed-secret.yaml
+    < secret.yaml > sealed-secret.yaml
 
-cat github-credentials.yaml | kubeseal --controller-namespace kube-system \
+kubeseal --controller-namespace kube-system \
     --controller-name sealed-secrets-sealed-secrets-controller \
     --format yaml \
-    > github-credentails-sealed-secret.yaml
+    < github-credentials.yaml > github-credentails-sealed-secret.yaml
