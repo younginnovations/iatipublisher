24031c40c91c29cf2d489b3f98b0fa951efaeb2c
sriza <sriza054@gmail.com>
2022-10-11 12:38:15 +0545

    add more narrative and element fixes

--- app/IATI/Elements/Forms/WrapperCollectionForm.php
+++ app/IATI/Elements/Forms/WrapperCollectionForm.php
@@ -74,7 +74,7 @@ class WrapperCollectionForm extends Form
 
                 if (isset($field['add_more']) && $field['add_more']) {
                     $this->add('add_to_collection_' . $name, 'button', [
-                        'label' => sprintf('Add More %s', str_replace('_',' ',Arr::get($data, 'attributes', null) && strtolower($field['name']) !=='narrative'? $field['name']:$data['name'])),
+                        'label' => sprintf('Add More %s', str_replace('_',' ',Arr::get($data, 'attributes', null) ? $field['name']:$data['name'])),
                         'attr'  => [
                             'class'     => 'add_to_collection add_more button relative -translate-y-1/2 pl-3.5 text-xs font-bold uppercase leading-normal text-spring-50 text-bluecoral',
                             'form_type' => $data['parent'] . '_' . $data['name'] . '_' . $field['name'],
