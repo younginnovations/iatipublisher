26ce7a4c6aeb6a79658198735787b8e48a5c73cb
Salil Rajkarnikar <rajkarnikarsalil11@gmai.com>
2022-11-23 13:07:00 +0545

    773-error-message-popup
    - [x] error message showing in popup
    - [x] error pop up fixed
    - [x] code fixes
    - [x] toast error position and scroll issue fix
    - [x] disisabled scroll when model is open
    - [x] error toast pos fix
    - [x] outlines removed
    - [x] popup title fixed

---
+++ resources/assets/js/components/ErrorPopUp.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <div @click="close" class="fixed inset-0 z-40 bg-black/20"></div>
+    <div
+      class="fixed top-[50vh] left-1/2 z-50 w-[500px] max-w-[90%] -translate-y-1/2 -translate-x-1/2 rounded-lg bg-white p-8"
+    >
+      <h3 class="mb-4 text-lg font-medium">
+        <svg-vue icon="alert" class="mr-2 inline text-crimson-40"></svg-vue
+        ><span>{{ props.title }}</span>
+      </h3>
+      <ul class="list-disc rounded-md bg-salmon-10 p-3 font-medium">
+        <li
+          v-for="(item, index) in props.message"
+          :key="index"
+          class="my-3 ml-6"
+        >
+          {{ item }}
+        </li>
+      </ul>
+      <div class="mt-4 flex flex-row-reverse">
+        <button
+          class="rounded bg-bluecoral py-2 px-5 font-semibold text-white"
+          @click="close"
+        >
+          Close
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import { defineProps, defineEmits, onMounted, onUnmounted, ref } from 'vue';
+
+const emit = defineEmits(['close-popup']);
+const props = defineProps({
+  message: { required: true },
+  title: { type: String, required: true },
+});
+const close = () => {
+  emit('close-popup', 'closed');
+};
+onMounted(() => {
+  document.documentElement.style.overflow = 'hidden';
+});
+onUnmounted(() => {
+  document.documentElement.style.overflow = 'auto';
+});
+</script>
