ff6a5228a18388a2a8b6b8008ae10a30f2d5b9be
Sanil Manandhar <sanilmanandhar@gmail.com>
2022-12-21 11:53:44 +0545

    Download all and download filtered backend complete

--- app/Http/Controllers/Admin/Download/DownloadActivityController.php
+++ app/Http/Controllers/Admin/Download/DownloadActivityController.php
@@ -60,24 +60,24 @@ class DownloadActivityController extends Controller
     public function downloadActivityCsv(Request $request): BinaryFileResponse|JsonResponse
     {
         try {
-            $activityIds = $request->get('activities') ?
+            $activityIds = ($request->get('activities') && $request->get('activities') !== 'all') ?
                 json_decode($request->get('activities'), true, 512, JSON_THROW_ON_ERROR) : [];
+            $headers = $this->downloadActivityService->getCsvHeaderArray('Activity', 'other_fields_transaction');
+            $filename = $this->downloadActivityService->getDownloadFilename($this->downloadActivityService->getOrganizationPublisherId());
 
-            if (is_array($activityIds) && !empty($activityIds)) {
+            if (request()->get('activities') === 'all') {
+                $activities = $this->downloadActivityService->getAllActivitiesToDownload($this->sanitizeRequest($request));
+            } elseif (is_array($activityIds) && !empty($activityIds)) {
                 $activities = $this->downloadActivityService->getActivitiesToDownload($activityIds);
-
-                if (!count($activities)) {
-                    return response()->json(['success' => false, 'message' => 'No activities selected.']);
-                }
-
-                $csvData = $this->downloadActivityService->getCsvData($activities);
-                $headers = $this->downloadActivityService->getCsvHeaderArray('Activity', 'other_fields_transaction');
-                $filename = $this->downloadActivityService->getDownloadFilename($this->downloadActivityService->getOrganizationPublisherId());
-
-                return $this->csvGenerator->generateWithHeaders($filename, $csvData, $headers);
             }
 
-            return response()->json(['success' => false, 'message' => 'No activities selected.']);
+            if (!isset($activities) || !count($activities)) {
+                return response()->json(['success' => false, 'message' => 'No activities selected.']);
+            }
+
+            $csvData = $this->downloadActivityService->getCsvData($activities);
+
+            return $this->csvGenerator->generateWithHeaders($filename, $csvData, $headers);
         } catch (\Exception $e) {
             logger()->error($e->getMessage());
 
@@ -96,38 +96,65 @@ class DownloadActivityController extends Controller
     public function downloadActivityXml(Request $request, bool $download = false): Response|JsonResponse
     {
         try {
-            $activityIds = $request->get('activities') ?
+            $activityIds = ($request->get('activities') && $request->get('activities') !== 'all') ?
                 json_decode($request->get('activities'), true, 512, JSON_THROW_ON_ERROR) : [];
+            $filename = $this->downloadActivityService->getDownloadFilename($this->downloadActivityService->getOrganizationPublisherId());
 
-            if (is_array($activityIds) && !empty($activityIds)) {
+            if (request()->get('activities') === 'all') {
+                $activities = $this->downloadActivityService->getAllActivitiesToDownload($this->sanitizeRequest($request));
+            } elseif (is_array($activityIds) && !empty($activityIds)) {
                 $activities = $this->downloadActivityService->getActivitiesToDownload($activityIds);
-
-                if (!count($activities)) {
-                    return response()->json(['success' => false, 'message' => 'No activities selected.']);
-                }
-
-                $mergedContent = $this->downloadActivityService->getCombinedXmlFile($activities);
-                $filename = $this->downloadActivityService->getDownloadFilename($this->downloadActivityService->getOrganizationPublisherId());
-
-                if (!$download && !$this->xmlServiceProvider->isValidAgainstSchema($mergedContent)) {
-                    return response()->json(['success' => false, 'xml_error' => true, 'message' => json_encode(libxml_get_errors(), JSON_THROW_ON_ERROR)]);
-                }
-
-                return response($mergedContent)
-                    ->withHeaders([
-                        'Content-Type' => 'text/xml',
-                        'Cache-Control' => 'public',
-                        'Content-Description' => 'File Transfer',
-                        'Content-Disposition' => 'attachment; filename=' . $filename . '.xml',
-                        'Content-Transfer-Encoding' => 'binary',
-                    ]);
             }
 
-            return response()->json(['success' => false, 'message' => 'No activities selected.']);
+            if (!isset($activities) || !count($activities)) {
+                return response()->json(['success' => false, 'message' => 'No activities selected.']);
+            }
+
+            $mergedContent = $this->downloadActivityService->getCombinedXmlFile($activities);
+
+            if (!$download && !$this->xmlServiceProvider->isValidAgainstSchema($mergedContent)) {
+                return response()->json(['success' => false, 'xml_error' => true, 'message' => json_encode(libxml_get_errors(), JSON_THROW_ON_ERROR)]);
+            }
+
+            return response($mergedContent)
+                ->withHeaders([
+                    'Content-Type' => 'text/xml',
+                    'Cache-Control' => 'public',
+                    'Content-Description' => 'File Transfer',
+                    'Content-Disposition' => 'attachment; filename=' . $filename . '.xml',
+                    'Content-Transfer-Encoding' => 'binary',
+                ]);
         } catch (\Exception $e) {
             logger()->error($e->getMessage());
 
             return response()->json(['success' => false, 'message' => 'Error has occurred while downloading activity csv.']);
         }
+    }
+
+    /**
+     * Sanitizes the request for removing code injections.
+     *
+     * @param $request
+     *
+     * @return array
+     */
+    public function sanitizeRequest($request): array
+    {
+        $tableConfig = getTableConfig('activity');
+        $queryParams = [];
+
+        if (!empty($request->get('q')) || $request->get('q') === '0') {
+            $queryParams['query'] = $request->get('q');
+        }
+
+        if (in_array($request->get('orderBy'), $tableConfig['orderBy'], true)) {
+            $queryParams['orderBy'] = $request->get('orderBy');
+
+            if (in_array($request->get('direction'), $tableConfig['direction'], true)) {
+                $queryParams['direction'] = $request->get('direction');
+            }
+        }
+
+        return $queryParams;
     }
 }
