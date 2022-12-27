b20c9b93a41ade92270d9230397295598b303ecf
Salil Rajkarnikar <rajkarnikarsalil11@gmai.com>
2022-12-27 17:47:06 +0545

    wip

--- resources/assets/js/components/sections/DeleteAction.vue
+++ resources/assets/js/components/sections/DeleteAction.vue
@@ -21,7 +21,7 @@
         <b v-else>Delete</b>
       </div>
       <div class="rounded-lg bg-rose p-4">
-<<<<<<< HEAD
+
         <p v-if="props.itemType === 'result'">
           Are you sure you want to delete this Result? Related Indicators and
           Periods will also be deleted
@@ -37,23 +37,6 @@
           Are you sure you want to delete this Transaction?
         </p>
         <p v-else>Are you sure you want to delete this module?</p>
-=======
-        <b v-if="props.itemType === 'result'"
-          >Are you sure you want to delete this Result? Related Indicators and
-          Periods will also be deleted</b
-        >
-        <b v-else-if="props.itemType === 'indicator'"
-          >Are you sure you want to delete this Indicator? Related Periods will
-          also be deleted</b
-        >
-        <b v-else-if="props.itemType === 'period'"
-          >Are you sure you want to delete this Period?</b
-        >
-        <b v-else-if="props.itemType === 'transaction'"
-          >Are you sure you want to delete this Transaction?</b
-        >
-        <b v-else>Are you sure you want to delete this module?</b>
->>>>>>> 58ff179c (row click issue fix)
       </div>
     </div>
     <div class="flex justify-end">
