7980f4359b0a716ff3e98f1417be4aa0f47d3fc4
Salil Rajkarnikar <rajkarnikarsalil11@gmai.com>
2023-01-05 17:11:33 +0545

    error borders fixed

--- resources/assets/js/views/user/UserProfile.vue
+++ resources/assets/js/views/user/UserProfile.vue
@@ -3,14 +3,17 @@
     <Loader v-if="isLoaderVisible" />
     <nav aria-label="breadcrumbs" class="rank-math-breadcrumb my-4">
       <div class="flex">
-        <a class="whitespace-nowrap font-bold" href="/activities"> Your Activities </a>
+        <a class="whitespace-nowrap font-bold" href="/activities">
+          Your Activities
+        </a>
         <span class="separator mx-4"> / </span>
         <div class="breadcrumb__title">
-          <span class="breadcrumb__title last max-w-lg overflow-hidden text-n-30">{{
-            user.full_name ?? "Untitled"
-          }}</span>
+          <span
+            class="breadcrumb__title last max-w-lg overflow-hidden text-n-30"
+            >{{ user.full_name ?? 'Untitled' }}</span
+          >
           <span class="ellipsis__title--hover w-[calc(100%_+_35px)]">{{
-            user.full_name ? user.full_name : "Untitled"
+            user.full_name ? user.full_name : 'Untitled'
           }}</span>
         </div>
       </div>
@@ -50,7 +53,9 @@
     <div class="my-4 rounded-lg bg-white p-8">
       <PopupModal :modal-active="editPasswordForm">
         <div class="popup-model h-auto">
-          <div class="mb-4 text-2xl font-bold text-bluecoral">Change Password</div>
+          <div class="mb-4 text-2xl font-bold text-bluecoral">
+            Change Password
+          </div>
           <div>
             <div class="mb-5 flex flex-col gap-2">
               <label class="text-sm text-n-50"
@@ -67,17 +72,22 @@
                   "
                 ></svg-vue>
                 <input
+                  :class="
+                    errorPasswordData.current_password !== ''
+                      ? 'border-crimson-50'
+                      : 'border-n-30'
+                  "
                   v-model="passwordData.current_password"
                   class="w-full rounded border border-n-30 p-3"
                   :type="showCurrentPassword ? 'text' : 'password'"
                 />
-                <span
-                  v-if="errorPasswordData.current_password !== ''"
-                  class="error"
-                  role="alert"
-                >
-                  {{ errorPasswordData.current_password }}
-                </span>
+              </span>
+              <span
+                v-if="errorPasswordData.current_password !== ''"
+                class="error"
+                role="alert"
+              >
+                {{ errorPasswordData.current_password }}
               </span>
             </div>
           </div>
@@ -97,14 +107,22 @@
                   "
                 ></svg-vue>
                 <input
+                  :class="
+                    errorPasswordData.password !== ''
+                      ? 'border-crimson-50'
+                      : 'border-n-30'
+                  "
                   v-model="passwordData.password"
                   class="w-full rounded border border-n-30 p-3"
                   :type="showNewPassword ? 'text' : 'password'"
-                />
-                <span v-if="errorPasswordData.password !== ''" class="error" role="alert">
-                  {{ errorPasswordData.password }}
-                </span></span
+                /> </span
+              ><span
+                v-if="errorPasswordData.password !== ''"
+                class="error"
+                role="alert"
               >
+                {{ errorPasswordData.password }}
+              </span>
             </div>
             <div class="flex w-full flex-col gap-2">
               <label class="text-sm text-n-50"
@@ -121,18 +139,22 @@
                   "
                 ></svg-vue
                 ><input
+                  :class="
+                    errorPasswordData.password_confirmation !== ''
+                      ? 'border-crimson-50'
+                      : 'border-n-30'
+                  "
                   v-model="passwordData.password_confirmation"
-                  class="w-full rounded border border-n-30 p-3"
+                  class="w-full rounded border p-3"
                   :type="showConfirmPassword ? 'text' : 'password'"
-                />
-                <span
-                  v-if="errorPasswordData.password_confirmation !== ''"
-                  class="error"
-                  role="alert"
-                >
-                  {{ errorPasswordData.password_confirmation }}
-                </span></span
+                /> </span
+              ><span
+                v-if="errorPasswordData.password_confirmation !== ''"
+                class="error"
+                role="alert"
               >
+                {{ errorPasswordData.password_confirmation }}
+              </span>
             </div>
           </div>
           <div class="mt-6 flex justify-end space-x-2">
@@ -146,25 +168,38 @@
             >
               Cancel
             </button>
-            <button class="primary-btn !px-10" @click="updatePassword">Save</button>
+            <button class="primary-btn !px-10" @click="updatePassword">
+              Save
+            </button>
           </div>
         </div>
       </PopupModal>
       <!-- profile edit popup form -->
       <PopupModal :modal-active="editProfileForm">
         <div class="popup-model">
-          <div class="mb-4 text-2xl font-bold text-bluecoral">Edit your profile</div>
+          <div class="mb-4 text-2xl font-bold text-bluecoral">
+            Edit your profile
+          </div>
           <div class="grid grid-cols-2 gap-6">
             <div class="col-span-2 flex flex-col items-start gap-2">
               <label class="text-sm text-n-50"
                 >Full Name<span class="text-[red]"> * </span></label
               >
               <input
+                :class="
+                  errorFormData.full_name !== ''
+                    ? 'border-crimson-50'
+                    : 'border-n-30'
+                "
                 v-model="formData.full_name"
                 class="w-full rounded border border-n-30 p-3"
                 type="text"
               />
-              <span v-if="errorFormData.full_name !== ''" class="error" role="alert">
+              <span
+                v-if="errorFormData.full_name !== ''"
+                class="error"
+                role="alert"
+              >
                 {{ errorFormData.full_name }}
               </span>
             </div>
@@ -173,11 +208,20 @@
                 >Username<span class="text-[red]"> * </span></label
               >
               <input
+                :class="
+                  errorFormData.username !== ''
+                    ? 'border-crimson-50'
+                    : 'border-n-30'
+                "
                 v-model="formData.username"
                 class="w-full rounded border border-n-30 p-3"
                 type="text"
               />
-              <span v-if="errorFormData.username !== ''" class="error" role="alert">
+              <span
+                v-if="errorFormData.username !== ''"
+                class="error"
+                role="alert"
+              >
                 {{ errorFormData.username }}
               </span>
             </div>
@@ -187,15 +231,27 @@
                 >Email<span class="text-[red]"> * </span></label
               >
               <input
+                :class="
+                  errorFormData.email !== ''
+                    ? 'border-crimson-50'
+                    : 'border-n-30'
+                "
                 v-model="formData.email"
                 class="w-full rounded border border-n-30 p-3"
                 type="email"
               />
-              <span v-if="errorFormData.email !== ''" class="error" role="alert">
+              <span
+                v-if="errorFormData.email !== ''"
+                class="error"
+                role="alert"
+              >
                 {{ errorFormData.email }}
               </span>
             </div>
-            <div class="flex flex-col items-start gap-2">
+            <div
+              :class="errorFormData.language_preference !== '' && 'error'"
+              class="flex flex-col items-start gap-2"
+            >
               <label class="text-sm text-n-50"
                 >Language Preference<span class="text-[red]">*</span></label
               >
@@ -225,7 +281,9 @@
             >
               Cancel
             </button>
-            <button class="primary-btn !px-10" @click="updateProfile">Save</button>
+            <button class="primary-btn !px-10" @click="updateProfile">
+              Save
+            </button>
           </div>
         </div>
       </PopupModal>
@@ -270,9 +328,12 @@
         <div class="text-base font-bold text-n-40">Email</div>
         <div>
           <a>{{ user.email }}</a>
-          <div v-if="!user.email_verified_at" class="mt-1 max-w-[550px] text-n-40">
-            You haven't verified your email address yet. Please check for verification
-            email sent to you and verify your account,
+          <div
+            v-if="!user.email_verified_at"
+            class="mt-1 max-w-[550px] text-n-40"
+          >
+            You haven't verified your email address yet. Please check for
+            verification email sent to you and verify your account,
             <a
               class="cursor-pointer font-bold underline"
               @click="resendVerificationEmail()"
@@ -286,14 +347,14 @@
   </div>
 </template>
 <script setup lang="ts">
-import { defineProps, reactive, ref } from "vue";
-import Loader from "../../components/Loader.vue";
-import PageTitle from "Components/sections/PageTitle.vue";
-import Toast from "Components/ToastMessage.vue";
-import axios from "axios";
-import PopupModal from "Components/PopupModal.vue";
-import encrypt from "Composable/encryption";
-import Multiselect from "@vueform/multiselect";
+import { defineProps, reactive, ref } from 'vue';
+import Loader from '../../components/Loader.vue';
+import PageTitle from 'Components/sections/PageTitle.vue';
+import Toast from 'Components/ToastMessage.vue';
+import axios from 'axios';
+import PopupModal from 'Components/PopupModal.vue';
+import encrypt from 'Composable/encryption';
+import Multiselect from '@vueform/multiselect';
 
 const props = defineProps({
   user: { type: Object, required: true },
@@ -302,7 +363,7 @@ const props = defineProps({
 
 const toastData = reactive({
   visibility: false,
-  message: "",
+  message: '',
   type: true,
 });
 const isLoaderVisible = ref(false);
@@ -320,29 +381,29 @@ const formData = reactive({
 });
 
 const errorFormData = reactive({
-  username: "",
-  full_name: "",
-  email: "",
-  language_preference: "",
+  username: '',
+  full_name: '',
+  email: '',
+  language_preference: '',
 });
 
 const passwordData = reactive({
-  current_password: "",
-  password: "",
-  password_confirmation: "",
+  current_password: '',
+  password: '',
+  password_confirmation: '',
 });
 
 const errorPasswordData = reactive({
-  current_password: "",
-  password: "",
-  password_confirmation: "",
+  current_password: '',
+  password: '',
+  password_confirmation: '',
 });
 
 const resendVerificationEmail = () => {
   isLoaderVisible.value = true;
 
   axios
-    .post("/user/verification/email")
+    .post('/user/verification/email')
     .then((res) => {
       toastData.visibility = true;
       toastData.message = res.data.message;
@@ -361,18 +422,21 @@ const updatePassword = () => {
   let passwordFormData = {
     current_password: encrypt(
       passwordData.current_password,
-      process.env.MIX_ENCRYPTION_KEY ?? ""
+      process.env.MIX_ENCRYPTION_KEY ?? ''
     ),
-    password: encrypt(passwordData.password, process.env.MIX_ENCRYPTION_KEY ?? ""),
+    password: encrypt(
+      passwordData.password,
+      process.env.MIX_ENCRYPTION_KEY ?? ''
+    ),
     password_confirmation: encrypt(
       passwordData.password_confirmation,
-      process.env.MIX_ENCRYPTION_KEY ?? ""
+      process.env.MIX_ENCRYPTION_KEY ?? ''
     ),
-    form_type: "password",
+    form_type: 'password',
   };
 
   axios
-    .post("/update/password", passwordFormData)
+    .post('/update/password', passwordFormData)
     .then((res) => {
       toastData.visibility = true;
       toastData.message = res.data.message;
@@ -382,7 +446,7 @@ const updatePassword = () => {
       if (res.data.success) {
         editPasswordForm.value = false;
         for (const key in errorPasswordData) {
-          errorPasswordData[key] = "";
+          errorPasswordData[key] = '';
         }
         logout();
       } else {
@@ -401,7 +465,7 @@ const updatePassword = () => {
 
 const updateProfile = () => {
   axios
-    .post("/update/profile", formData)
+    .post('/update/profile', formData)
     .then((res) => {
       toastData.visibility = true;
       toastData.message = res.data.message;
@@ -411,7 +475,7 @@ const updateProfile = () => {
       if (res.data.success) {
         editProfileForm.value = false;
         for (const key in errorFormData) {
-          errorFormData[key] = "";
+          errorFormData[key] = '';
         }
       } else {
         for (const key in res.data.errors) {
@@ -428,9 +492,9 @@ const updateProfile = () => {
 };
 
 async function logout() {
-  await axios.post("/logout").then((res) => {
+  await axios.post('/logout').then((res) => {
     if (res.status) {
-      window.location.href = "/";
+      window.location.href = '/';
     }
   });
 }
