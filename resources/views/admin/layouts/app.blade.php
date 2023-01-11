eadb4810fcac2162e0b9b242ca75fae0cb090de3
Salil Rajkarnikar <rajkarnikarsalil11@gmai.com>
2023-01-11 15:02:31 +0545

    gtags added

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -5,6 +5,9 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
+  <!-- Google tag (gtag.js) -->
+  <script defer data-domain="[publisher.iatistandard.org](http://publisher.iatistandard.org/)" src="https://plausible.io/js/script.js"></script>
+
   <!-- CSRF Token -->
   <meta name="csrf-token" content="{{ csrf_token() }}">
 
