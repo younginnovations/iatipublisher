04a03dbfc6ca8628e90ebeb507ed0cbcdeedf98b
sriza <sriza054@gmail.com>
2023-01-16 08:18:54 +0545

    migration fixes

---
+++ database/migrations/2023_01_16_095827_add_created_by_updated_by_to_activity_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Constants\DBTables;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table(DBTables::ACTIVITY, function (Blueprint $table) {
+            $table->integer('created_by');
+            $table->integer('updated_by');
+            $table->foreign('created_by')->references('id')->on(DBTables::USERS)->onDelete('cascade');
+            $table->foreign('updated_by')->references('id')->on(DBTables::USERS)->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table(DBTables::ACTIVITY, function (Blueprint $table) {
+            $table->dropColumn('created_by');
+            $table->dropColumn('updated_by');
+        });
+    }
+};
