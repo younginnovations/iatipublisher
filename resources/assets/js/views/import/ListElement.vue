fd640c6369845c9e407897c93d59977e618e2b33
Salil Rajkarnikar <rajkarnikarsalil11@gmai.com>
2023-02-07 16:05:34 +0545

    - [x] ui implementation - [x] font fix - [x] icons added

--- resources/assets/js/views/import/ListElement.vue
+++ resources/assets/js/views/import/ListElement.vue
@@ -1,36 +1,127 @@
 <template>
   <td class="title">
-    {{
-      activity['data']['title'][0]['narrative']
-        ? activity['data']['title'][0]['narrative']
-        : 'Missing'
-    }}
+    <span class="flex justify-between">
+      <span class="font-normal">{{
+        activity['data']['title'][0]['narrative']
+          ? activity['data']['title'][0]['narrative']
+          : 'Missing'
+      }}</span>
 
-    <span
-      v-if="Object.keys(activity['errors']).length > 0"
-      class="inline-flex cursor-pointer items-center text-crimson-50"
-      @click="toggleError"
-    >
-      Show {{ countErrors() }} errors
-      <svg-vue
-        icon="dropdown-arrow"
-        class="ml-1 text-[4px]"
-        :class="{ 'rotate-180': active, '': !active }"
-      />
-    </span>
+      <span
+        v-if="Object.keys(activity['errors']).length > 0"
+        class="mb-4 ml-4 inline-flex cursor-pointer items-center text-sm font-medium text-crimson-50"
+        @click="toggleError"
+      >
+        <span class="flex items-center space-x-2">
+          <svg-vue class="text-crimson-40" icon="alert" />
+          <span> Show {{ countErrors() }} Issues</span></span
+        >
 
-    <div class="upload-error-content" :class="{ open: active }">
-      <div v-for="(ele_err, i) in activity['errors']" :key="i">
+        <svg-vue
+          icon="dropdown-arrow"
+          class="ml-1 text-[4px] duration-200"
+          :class="{ 'rotate-180': active, '': !active }"
+        /> </span
+    ></span>
+
+    <div class="upload-error-content duration-200" :class="{ closed: !active }">
+      <div class="p-4">
+        <div class="critical-container mt-2">
+          <div
+            class="flex items-center justify-between border border-none bg-rose p-3"
+            @click="
+              () => {
+                showCritical = !showCritical;
+              }
+            "
+          >
+            <span class="flex items-center space-x-2">
+              <svg-vue class="text-crimson-40" icon="alert" />
+              <span> Critical errors</span></span
+            >
+            <svg-vue
+              icon="dropdown-arrow"
+              class="ml-1 cursor-pointer text-[4px] duration-200"
+              :class="{ 'rotate-180': showCritical, '': !showCritical }"
+            />
+          </div>
+          <div class="error-dropdown" :class="showCritical ? '' : 'hide-error'">
+            <div
+              class="bg-rose p-4"
+              v-for="(ele_err, i) in activity['errors']['critical']"
+              :key="i"
+            >
+              <p class="mb-2 font-semibold capitalize">
+                {{ Object.keys(activity['errors']['critical'])[+i] }}{{ i }}
+              </p>
+
+              <p
+                v-for="item in Object.values(ele_err)"
+                :key="(item as string)"
+                class="error-list text-sm font-medium"
+              >
+                {{ item }}
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="warning-container my-2 border-none">
+          <div
+            class="flex items-center justify-between bg-eggshell p-3"
+            @click="
+              () => {
+                showWarning = !showWarning;
+              }
+            "
+          >
+            <span class="flex items-center space-x-2">
+              <svg-vue icon="alert" class="text-camel-40" /><span
+                >Warnings</span
+              ></span
+            >
+            <svg-vue
+              icon="dropdown-arrow"
+              class="ml-1 text-[4px] duration-200"
+              :class="{ 'rotate-180': showWarning, '': !showWarning }"
+            />
+          </div>
+          <div :class="showWarning ? '' : 'hide-error'" class="error-dropdown">
+            <div
+              class="bg-eggshell p-4"
+              v-for="(ele_err, i) in activity['errors']['warning']"
+              :key="i"
+            >
+              <p class="mb-2 font-semibold capitalize">
+                {{ Object.keys(activity['errors']['warning'])[+i] }}{{ i }}
+              </p>
+
+              <p
+                v-for="item in Object.values(ele_err)"
+                :key="(item as string)"
+                class="error-list text-sm font-medium"
+              >
+                {{ item }}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- <div v-for="(ele_err, i) in activity['errors']" :key="i">
         <ul>
           <li v-for="(err, key, j) in ele_err" :key="j">
-            <p>
+            <p class="mb-2 font-semibold capitalize">
               {{ key.toString().replace(/_/g, ' ').replace(/\./g, ' > ') }}
             </p>
-            <!-- eslint-disable-next-line vue/no-v-html -->
-            <p v-html="err"></p>
+            <p
+              v-for="item in Object.values(err)"
+              :key="(item as string)"
+              class="error-list"
+            >
+              {{ item }}
+            </p>
           </li>
         </ul>
-      </div>
+      </div> -->
     </div>
   </td>
 
@@ -58,7 +149,7 @@
 </template>
 
 <script setup lang="ts">
-import { defineProps, defineEmits, ref, watch, reactive } from 'vue';
+import { defineProps, defineEmits, ref, watch, reactive, onUpdated } from 'vue';
 
 const props = defineProps({
   activity: {
@@ -78,6 +169,8 @@ const props = defineProps({
 const emit = defineEmits(['selectElement']);
 
 const active = ref(false);
+const showCritical = ref(false);
+const showWarning = ref(false);
 let activities = reactive([]);
 function toggleError() {
   active.value = !active.value;
@@ -107,4 +200,44 @@ watch(
     }
   }
 );
+onUpdated(() => {
+  console.log(Object.keys(props.activity['errors']['critical'])[0]);
+});
 </script>
+<style scoped>
+.error-dropdown {
+  overflow: hidden;
+  transition: max-height 0.3s ease-out;
+  height: auto;
+  max-height: 600px;
+}
+.error-dropdown.hide-error {
+  max-height: 0;
+}
+.critical-container {
+  position: relative;
+}
+.critical-container::after {
+  position: absolute;
+  content: ' ';
+  z-index: 10;
+  @apply bg-crimson-40;
+  height: 100%;
+  width: 2px;
+  left: 0;
+  top: 0;
+}
+.warning-container {
+  position: relative;
+}
+.warning-container::after {
+  position: absolute;
+  content: ' ';
+  z-index: 10;
+  @apply bg-camel-40;
+  height: 100%;
+  width: 2px;
+  left: 0;
+  top: 0;
+}
+</style>
