592cd8210c0cbcd30ef7691dc434c60f57ba8bc9
sriza <sriza054@gmail.com>
2023-02-13 17:10:04 +0545

    php docs added

--- app/Providers/ExtendedMailServiceProvider.php
+++ app/Providers/ExtendedMailServiceProvider.php
@@ -5,8 +5,16 @@ namespace App\Providers;
 use Illuminate\Mail\MailServiceProvider;
 use Illuminate\Support\Facades\Mail;
 
+/**
+ * Class ExtendedMailServiceProvider.
+ */
 class ExtendedMailServiceProvider extends MailServiceProvider
 {
+    /**
+     * Bootstrap mail service.
+     *
+     * @return void
+     */
     public function boot()
     {
         if (!app()->isProduction()) {
