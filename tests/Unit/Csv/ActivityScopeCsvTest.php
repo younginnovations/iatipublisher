b2d1a01e2f08bc6db0728c6be90fa38bb7438105
aashish.thapamagar <aashish.thapamagar@ekbana.info>
2023-02-28 17:25:39 +0545

    -[x] test: Activity scope csv import unit test complete

---
+++ tests/Unit/Csv/ActivityScopeCsvTest.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Tests\Unit\Csv;
+
+use App\CsvImporter\Entities\Activity\Components\Elements\ActivityScope;
+use Illuminate\Support\Arr;
+
+class ActivityScopeCsvTest extends CsvBaseTest
+{
+    /**
+     * All Data invalid.
+     *
+     * @return void
+     * @test
+     * @throws \JsonException
+     */
+    public function check_throws_validation_when_invalid_data(): void
+    {
+        $this->signIn();
+        $rows = $this->invalid_activity_scope_data();
+        $errors = [];
+
+        foreach ($rows as $row) {
+            $element = new ActivityScope($row, $this->validation);
+            $element->validate()->withErrors();
+
+            if (!empty($element->errors()) || !empty($element->criticals()) || !empty($element->warnings())) {
+                $errors[] = $element->errors() + $element->criticals() + $element->warnings();
+            }
+        }
+
+        $flattenErrors = Arr::flatten($errors);
+        $this->assertContains('The activity scope cannot have more than one value.', $flattenErrors);
+        $this->assertContains('The activity scope does not exist.', $flattenErrors);
+    }
+
+    /**
+     * @return array
+     */
+    public function invalid_activity_scope_data(): array
+    {
+        $data = $this->completeData;
+        $data[0]['activity_scope'] = [1, 2, 3];
+        $data[1]['activity_scope'] = [10];
+
+        return $data;
+    }
+
+    /**
+     * @return void
+     * @throws \JsonException
+     * @test
+     */
+    public function check_passes_if_valid_data(): void
+    {
+        $this->signIn();
+        $rows = $this->valid_activity_scope_data();
+        $errors = [];
+
+        foreach ($rows as $row) {
+            $element = new ActivityScope($row, $this->validation);
+            $element->validate()->withErrors();
+
+            if (!empty($element->errors()) || !empty($element->criticals()) || !empty($element->warnings())) {
+                $errors[] = $element->errors() + $element->criticals() + $element->warnings();
+            }
+        }
+
+        $flattenErrors = Arr::flatten($errors);
+        $this->assertEmpty($flattenErrors);
+    }
+
+    /**
+     * @return array
+     */
+    public function valid_activity_scope_data(): array
+    {
+        $data = $this->completeData;
+        $data[0]['activity_scope'] = [1];
+        $data[1]['activity_scope'] = [2];
+
+        return $data;
+    }
+}
