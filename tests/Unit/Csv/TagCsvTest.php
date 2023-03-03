25d9279fc025a767a78b3f403ce291e0b8da3379
aashish.thapamagar <aashish.thapamagar@ekbana.info>
2023-03-03 13:38:19 +0545

    -[x] test: TagCsvTest import unit test complete

---
+++ tests/Unit/Csv/TagCsvTest.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace Tests\Unit\Csv;
+
+use App\CsvImporter\Entities\Activity\Components\Elements\Tag;
+use Illuminate\Support\Arr;
+
+class TagCsvTest extends CsvBaseTest
+{
+    /**
+     * @param $rows
+     * @return array
+     * @throws \JsonException
+     */
+    public function getErrors($rows): array
+    {
+        $errors = [];
+        foreach ($rows as $row) {
+            $element = new Tag($row, $this->validation);
+            $element->validate()->withErrors();
+
+            if (!empty($element->errors()) || !empty($element->criticals()) || !empty($element->warnings())) {
+                $errors[] = $element->errors() + $element->criticals() + $element->warnings();
+            }
+        }
+
+        return $errors;
+    }
+
+   /**
+    * All Data Invalid.
+    *
+    * @return void
+    * @test
+    * @throws \JsonException
+    */
+   public function throw_validation_invalid_data(): void
+   {
+       $this->signIn();
+       $rows = $this->invalid_data();
+       $errors = $this->getErrors($rows);
+       $flattenErrors = Arr::flatten($errors);
+
+       $this->assertContains('The tag vocabulary is invalid.', $flattenErrors);
+       $this->assertContains('The tag SDG code is invalid', $flattenErrors);
+       $this->assertContains('The tag SDG targets code is invalid.', $flattenErrors);
+       $this->assertContains('The @vocabulary-uri field must be a valid url.', $flattenErrors);
+   }
+
+   /**
+    * @return array
+    */
+   public function invalid_data(): array
+   {
+       $data = $this->completeData;
+       $data[0]['tag_vocabulary'] = ['0987', '2', '3', '99'];
+       $data[0]['tag_code'] = ['invalid vocab', 'invalid code', 'invalid code', '3333'];
+       $data[0]['tag_vocabulary_uri'] = ['', '', '', 'invalid url'];
+       $data[0]['tag_narrative'] = ['nar1', 'narr2', 'narr3', 'narr 4'];
+
+       $data[1]['tag_vocabulary'] = ['0987', '2', '3', '99'];
+       $data[1]['tag_code'] = ['invalid vocab', 'invalid code', 'invalid code', '3333'];
+       $data[1]['tag_vocabulary_uri'] = ['', '', '', 'invalid url'];
+       $data[1]['tag_narrative'] = ['nar1', 'narr2', 'narr3', 'narr 4'];
+
+       return $data;
+   }
+
+   /**
+    * @return void
+    * @throws \JsonException
+    * @test
+    */
+   public function pass_if_all_valid_data(): void
+   {
+       $this->signIn();
+       $rows = $this->valid_data();
+       $errors = $this->getErrors($rows);
+       $flattenErrors = Arr::flatten($errors);
+       $this->assertEmpty($flattenErrors);
+   }
+
+   public function valid_data(): array
+   {
+       $data = $this->completeData;
+       $data[0]['tag_vocabulary'] = ['1', '2', '3', '99'];
+       $data[0]['tag_code'] = ['1234', '1', '1.1', '12345'];
+       $data[0]['tag_vocabulary_uri'] = ['', '', '', 'https://www.google.com'];
+       $data[0]['tag_narrative'] = ['nar1', 'narr2', 'narr3', 'narr 4'];
+
+       $data[1]['tag_vocabulary'] = ['1', '2', '3', '99'];
+       $data[1]['tag_code'] = ['1234', '1', '1.1', '12345'];
+       $data[1]['tag_vocabulary_uri'] = ['', '', '', 'https://www.google.com'];
+       $data[1]['tag_narrative'] = ['nar1', 'narr2', 'narr3', 'narr 4'];
+
+       return $data;
+   }
+}
