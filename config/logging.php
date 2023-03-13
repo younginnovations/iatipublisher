5449ae56c9b6ac0f3a86f04eebcf79113399996d
sriza <sriza054@gmail.com>
2023-03-13 09:41:29 +0545

    clearing logs created before 30 days

--- config/logging.php
+++ config/logging.php
@@ -64,7 +64,7 @@ return [
             'driver' => 'daily',
             'path' => storage_path('logs/laravel.log'),
             'level' => env('LOG_LEVEL', 'debug'),
-            'days' => 14,
+            'days' => 32,
         ],
 
         'slack' => [
