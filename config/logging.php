0d1bca0ed700f1286d53aab853255b5f3195259c
sriza <sriza054@gmail.com>
2023-03-13 10:39:55 +0545

    laravel configuration for logging changed to have 35 days of logs

--- config/logging.php
+++ config/logging.php
@@ -64,7 +64,7 @@ return [
             'driver' => 'daily',
             'path' => storage_path('logs/laravel.log'),
             'level' => env('LOG_LEVEL', 'debug'),
-            'days' => 32,
+            'days' => 35,
         ],
 
         'slack' => [
