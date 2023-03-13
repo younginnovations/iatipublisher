85cf8bd69e665984909d772c4d1ca02132a125a7
Sanil Manandhar <sanilmanandhar@gmail.com>
2023-03-13 17:57:40 +0545

    User migration completed and organization migration updated

--- app/IATI/Models/User/User.php
+++ app/IATI/Models/User/User.php
@@ -49,6 +49,7 @@ class User extends Authenticatable implements MustVerifyEmail, Auditable
         'status',
         'language_preference',
         'registration',
+        'migrated_from_aidstream',
     ];
 
     /**
