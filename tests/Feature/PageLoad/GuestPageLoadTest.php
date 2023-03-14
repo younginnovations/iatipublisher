9e56c54ead78dbb7d56f58773142ea3db8dd24e4
aashish.thapamagar <aashish.thapamagar@ekbana.info>
2023-03-14 22:36:48 +0545

    -[x] test: Page load test for guest pages

---
+++ tests/Feature/PageLoad/GuestPageLoadTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Tests\Feature\PageLoad;
+
+use Tests\TestCase;
+
+class GuestPageLoadTest extends TestCase
+{
+    /**
+     * Guest Page Load test.
+     *
+     * @return void
+     * @test
+     * @dataProvider guestUrl
+     */
+    public function check_page_loads_before_login($route, $params = null): void
+    {
+        $response = $this->get(route($route, $params));
+        $response->assertStatus(200);
+    }
+
+    /**
+     * List of guest urls.
+     *
+     * @return array
+     */
+    public function guestUrl(): array
+    {
+        return [
+            ['web.', 'Sign In.'],
+            ['web.index.login'],
+            ['web.join', ['page' => 'random-page']],
+            ['web.register'],
+            ['web.password.email'],
+            ['web.password.confirm'],
+            ['web.iati.register'],
+            ['about'],
+            ['publishingchecklist'],
+            ['iatistandard'],
+            ['support'],
+        ];
+    }
+}
