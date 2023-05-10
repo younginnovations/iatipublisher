d1c8c339934c6a6d39720cbf755138fc5263ef7d
PG-Momik <momik12339@gmail.com>
2023-05-10 10:36:45 +0545

    -[x] add indicative to recipient country budget status

---
+++ app/Console/Commands/FixIndicative.php
@@ -0,0 +1,165 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Console\Commands;
+
+use App\IATI\Services\Organization\OrganizationService;
+use App\IATI\Traits\MigrateActivityTrait;
+use App\IATI\Traits\MigrateGeneralTrait;
+use App\IATI\Traits\MigrateOrganizationTrait;
+use Illuminate\Console\Command;
+use Illuminate\Database\DatabaseManager;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
+use Throwable;
+
+class FixIndicative extends Command
+{
+    use MigrateOrganizationTrait;
+    use MigrateActivityTrait;
+    use MigrateGeneralTrait;
+
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'command:FixIndicative';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Adds indicative to recipient country budget >> status for migrated organizations';
+
+    public function __construct(
+        protected DB $db,
+        protected DatabaseManager $databaseManager,
+        protected OrganizationService $organizationService
+    ) {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     *
+     * @throws Throwable
+     */
+    public function handle(): void
+    {
+        try {
+            $aidstreamOrganizationIdString = $this->askValid(
+                'Enter aidstream organization Ids (csv): ',
+                'aidstreamOrganizationIdString',
+                ['required']
+            );
+
+            $aidstreamOrganizationIds = explode(',', $aidstreamOrganizationIdString);
+
+            foreach ($aidstreamOrganizationIds as $key => $aidstreamOrganizationId) {
+                $aidstreamOrganizationIds[$key] = (int) $aidstreamOrganizationId;
+            }
+
+            $this->databaseManager->beginTransaction();
+
+            $aidstreamOrganizationIdentifierArray = $this->getAidstreamOrganizationIdentifier($aidstreamOrganizationIds);
+            $aidstreamOrganizationIdentifierArray = array_map('strtolower', $aidstreamOrganizationIdentifierArray);
+            $iatiOrganizations = $this->organizationService->getOrganizationByPublisherIds($aidstreamOrganizationIdentifierArray);
+
+            foreach ($iatiOrganizations as $iatiOrganization) {
+                $recipientCountryBudget = $iatiOrganization->recipient_country_budget;
+                $recipientCountryBudget['status'] = '1';
+
+                $iatiOrganization->timestamps = false;
+                $iatiOrganization->updateQuietly(
+                    ['recipient_country_budget'=>$recipientCountryBudget],
+                    ['touch'                   =>false]
+                );
+            }
+
+            $this->databaseManager->commit();
+
+            $this->info('Completed updating organization.');
+        } catch(\Exception $e) {
+            $this->databaseManager->rollBack();
+            $this->logInfo($e->getMessage());
+            logger()->error($e);
+        }
+    }
+
+    /**
+     * Ask input from user and return value.
+     *
+     * @param $question
+     * @param $field
+     * @param $rules
+     *
+     * @return string
+     */
+    protected function askValid($question, $field, $rules): string
+    {
+        $value = $this->ask($question);
+        $message = $this->validateInput($rules, $field, $value);
+
+        if ($message) {
+            $this->error($message);
+
+            return $this->askValid($question, $field, $rules);
+        }
+
+        return $value;
+    }
+
+    /**
+     * Validates input given by user.
+     *
+     * @param $rules
+     * @param $fieldName
+     * @param $value
+     *
+     * @return string|null
+     */
+    protected function validateInput($rules, $fieldName, $value): ?string
+    {
+        $validator = Validator::make([
+            $fieldName => $value,
+        ], [
+            $fieldName => $rules,
+        ]);
+
+        return $validator->fails()
+            ? $validator->errors()->first($fieldName)
+            : null;
+    }
+
+    /**
+     * Returns array of [organizationId => organizationIdentifier].
+     *
+     * @param $aidstreamOrganizationIds
+     *
+     * @return array
+     */
+    private function getAidstreamOrganizationIdentifier($aidstreamOrganizationIds): array
+    {
+        $returnArr = [];
+        $aidStreamSettings = $this->db::connection('aidstream')->table('settings')
+            ->whereIn('organization_id', $aidstreamOrganizationIds)
+            ->get();
+
+        if ($aidStreamSettings) {
+            foreach ($aidStreamSettings as $aidStreamSetting) {
+                if (in_array($aidStreamSetting->organization_id, $aidstreamOrganizationIds)) {
+                    $registryInfo = $aidStreamSetting->registry_info ? json_decode($aidStreamSetting->registry_info) : false;
+                    $organizationIdentifier = $registryInfo[0]?->publisher_id;
+                    $returnArr[$aidStreamSetting->organization_id] = $organizationIdentifier;
+                }
+            }
+        }
+
+        return $returnArr;
+    }
+}
