765cbfc107ab4ca919873b426e8ab461ff46dc95
Salil Rajkarnikar <rajkarnikarsalil11@gmai.com>
2023-05-17 17:28:19 +0545

    ui of dashboard

--- resources/assets/js/views/dashboard/DashboardPage.vue
+++ resources/assets/js/views/dashboard/DashboardPage.vue
@@ -1,19 +1,37 @@
 <template>
   <div class="py-8 px-6">
-    <div class="mb-3 outline">
+    <div class="mb-3">
       <div class="flex gap-x-2">
         <button
-          class="flex w-[140px] justify-center rounded border border-n-20 py-2"
+          :class="
+            currentView === 'publisher'
+              ? ' !border-turquoise bg-n-10 text-bluecoral'
+              : ''
+          "
+          class="flex w-[140px] justify-center rounded border border-n-20 py-2 text-sm text-n-40"
+          @click="currentView = 'publisher'"
         >
           <span> Publisher</span>
         </button>
         <button
-          class="flex w-[140px] justify-center rounded border border-n-20 py-2"
+          :class="
+            currentView === 'activity'
+              ? ' !border-turquoise bg-n-10 text-bluecoral'
+              : ''
+          "
+          class="flex w-[140px] justify-center rounded border border-n-20 py-2 text-sm text-n-40"
+          @click="currentView = 'activity'"
         >
           <span>Activity</span>
         </button>
         <button
-          class="flex w-[140px] justify-center rounded border border-n-20 py-2"
+          :class="
+            currentView === 'users'
+              ? ' !border-turquoise bg-n-10 text-bluecoral'
+              : ''
+          "
+          class="flex w-[140px] justify-center rounded border border-n-20 py-2 text-sm text-n-40"
+          @click="currentView = 'users'"
         >
           <span>Users</span>
         </button>
@@ -25,4 +43,6 @@
 
 <script setup lang="ts">
 import DashboardGraph from './DashboardGraph.vue';
+import { ref } from 'vue';
+const currentView = ref('publisher');
 </script>
