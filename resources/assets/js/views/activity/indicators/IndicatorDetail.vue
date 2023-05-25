44eb6165ba76d48c8c53a9c41a5cc996940a26e1
Salil Rajkarnikar <rajkarnikarsalil11@gmai.com>
2023-05-25 14:50:50 +0545

    text padding fix

--- resources/assets/js/views/activity/indicators/IndicatorDetail.vue
+++ resources/assets/js/views/activity/indicators/IndicatorDetail.vue
@@ -213,7 +213,7 @@
                 <div class="item elements-detail wider">
                   <table class="mb-2">
                     <tr>
-                      <td class="ml-4">Document Link:</td>
+                      <td class="pl-4">Document Link:</td>
                     </tr>
                   </table>
                 </div>
