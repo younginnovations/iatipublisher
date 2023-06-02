56056565ad2706feba2b840d14556e177528003c
aashish.thapamagar <aashish.thapamagar@ekbana.info>
2023-06-02 10:35:38 +0545

    -[x] feat: mapped activity transactions, result, indicator and period database index

--- app/IATI/Traits/IatiValidatorResponseTrait.php
+++ app/IATI/Traits/IatiValidatorResponseTrait.php
@@ -4,7 +4,8 @@ declare(strict_types=1);
 
 namespace App\IATI\Traits;
 
-use App\IATI\Models\Activity\Activity;
+use App\IATI\Services\Activity\ActivityService;
+use Illuminate\Contracts\Container\BindingResolutionException;
 use Illuminate\Support\Arr;
 
 trait IatiValidatorResponseTrait
@@ -47,10 +48,34 @@ trait IatiValidatorResponseTrait
         }
 
         $response['errors'] = $updatedErrors;
+        $this->clearXmlValidationOnS3($activity);
 
         return $response;
     }
 
+    /**
+     * Clears XML Validation file from s3.
+     *
+     * @param $activity
+     *
+     * @return void
+     */
+    public function clearXmlValidationOnS3($activity): void
+    {
+        awsDeleteFile("xmlValidation/$activity->org_id/activity_$activity->id.xml");
+        awsDeleteFile("xmlValidation/$activity->org_id/activity_mapper_$activity->id.xml");
+    }
+
+    /**
+     * Pinpoint exact parent xml node and generates url.
+     *
+     * @param $lineNumber
+     * @param $activity
+     *
+     * @return string|null
+     *
+     * @throws \JsonException
+     */
     public function getNodeByLineNumber($lineNumber, $activity): ?string
     {
         $lineNumber++;
@@ -90,21 +115,34 @@ trait IatiValidatorResponseTrait
             }
 
             if ($targetNode) {
-                return $this->generateUrlPath($targetNode, $activity->id);
+                return $this->generateUrlPath($targetNode, $activity);
             }
         }
 
         return null;
     }
 
-    public function generateUrlPath($targetNode, $activityId)
+    /**
+     * Generates url path to navigate the error.
+     *
+     * @param $targetNode
+     * @param $activity
+     *
+     * @return string
+     *
+     * @throws \JsonException
+     */
+    public function generateUrlPath($targetNode, $activity): string
     {
+        $activityId = $activity->id;
         $path = $targetNode;
         $subtractedPath = preg_replace_callback('/\[(\d+)\]/', static function ($matches) {
             return '[' . ($matches[1] - 1) . ']';
         }, $path);
         $explodedPath = explode('/', $subtractedPath);
         $firstElementName = str_replace('-', '_', preg_replace('/[^a-zA-Z-]/', '', $explodedPath[0]));
+        $activityMapperFile = awsGetFile("xmlValidation/$activity->org_id/activity_mapper_$activityId.xml");
+        $activityTransactionResultMapper = json_decode($activityMapperFile, true, 512, JSON_THROW_ON_ERROR);
         $getId = [];
         $urlPath = [];
 
@@ -124,26 +162,26 @@ trait IatiValidatorResponseTrait
         }
 
         if (array_key_exists('period', $getId)) {
-            $indicatorIdPath = 'results.' . $getId['result'] . '.indicators.' . $getId['indicator'];
-            $periodIdPath = $indicatorIdPath . '.periods.' . $getId['period'];
+            $indicatorIdPath = $activityTransactionResultMapper['indicators'][$getId['indicator']];
+            $periodIdPath = $activityTransactionResultMapper['periods'][$getId['period']];
             $indicatorId = $this->getElementId($indicatorIdPath, $activityId);
             $periodId = $this->getElementId($periodIdPath, $activityId);
             $url = array_flip($this->multilevelElements)['period'];
             $url = str_replace(['indicatorId', 'periodId'], [$indicatorId, $periodId], $url);
         } elseif (array_key_exists('indicator', $getId)) {
-            $resultIdPath = 'results.' . $getId['result'];
-            $indicatorIdPath = $resultIdPath . '.indicators.' . $getId['indicator'];
+            $resultIdPath = $activityTransactionResultMapper['results'][$getId['result']];
+            $indicatorIdPath = $activityTransactionResultMapper['indicators'][$getId['indicator']];
             $resultId = $this->getElementId($resultIdPath, $activityId);
             $indicatorId = $this->getElementId($indicatorIdPath, $activityId);
             $url = array_flip($this->multilevelElements)['indicator'];
             $url = str_replace(['resultId', 'indicatorId'], [$resultId, $indicatorId], $url);
         } elseif (array_key_exists('result', $getId)) {
-            $resultIdPath = 'results.' . $getId['result'];
+            $resultIdPath = $activityTransactionResultMapper['results'][$getId['result']];
             $resultId = $this->getElementId($resultIdPath, $activityId);
             $url = array_flip($this->multilevelElements)['result'];
             $url = str_replace(['activityId', 'resultId'], [$activityId, $resultId], $url);
         } elseif (array_key_exists('transaction', $getId)) {
-            $transactionIdPath = 'transactions.' . $getId['transaction'];
+            $transactionIdPath = $activityTransactionResultMapper['transactions'][$getId['transaction']];
             $transactionId = $this->getElementId($transactionIdPath, $activityId);
             $url = array_flip($this->multilevelElements)['transaction'];
             $url = str_replace(['activityId', 'transactionId'], [$activityId, $transactionId], $url);
@@ -157,9 +195,18 @@ trait IatiValidatorResponseTrait
         return $url . '#' . $urlId;
     }
 
-    public function getElementId($idPath, $activityId)
+    /**
+     * @param $idPath
+     * @param $activityId
+     *
+     * @return array|\ArrayAccess|mixed
+     *
+     * @throws BindingResolutionException
+     */
+    public function getElementId($idPath, $activityId): mixed
     {
-        $activity = Activity::with('results.indicators.periods', 'transactions')->where('id', $activityId)->first()->toArray();
+        $activityService = app()->make(ActivityService::class);
+        $activity = $activityService->getActivitityWithRelationsById($activityId)->toArray();
 
         return Arr::get($activity, $idPath . '.id');
     }
