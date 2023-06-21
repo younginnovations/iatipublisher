aa38e8d65c05f6512b125a04256ae6af1fa597c0
Sanil Manandhar <sanilmanandhar@gmail.com>
2023-06-21 13:46:58 +0545

    Updated aidstream conneciton in database.php

--- config/database.php
+++ config/database.php
@@ -96,7 +96,8 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
             'search_path' => 'public',
-            'sslmode' => 'disable',
+            'sslmode' => env('DB_SSLMODE', 'prefer'),
+            'sslrootcert' => env('DB_SSLROOTCERT', './ca-certificate.crt'),
             'pool_size' => 20,
             'options' => [
                 PDO::ATTR_PERSISTENT => true,
