785c8dec3db5488704c99e50316ba19e9889f920
sriza <sriza054@gmail.com>
2023-06-26 16:07:07 +0545

    removed commented code

--- resources/views/admin/activity/period/edit.blade.php
+++ resources/views/admin/activity/period/edit.blade.php
@@ -63,8 +63,6 @@
             <div class="hidden collection-container title_narrative" form_type="actual_dimension"
               data-prototype="{{ str_replace('actual[0]','actual[__PARENT_NAME__]',form_row($form->actual->getChildren()[0]->getChild('dimension')->prototype())) }}">
             </div>
-            {{ str_replace('actual[0]','actual[__PARENT_NAME__]',form_row($form->actual->getChildren()[0]->getChild('comment')->getChildren()[0]->getChild('narrative')->prototype())) }}
-            {{-- {{ str_replace('actual[0]','actual[__PARENT_NAME__]',form_row($form->target->getChildren()[0]->getChild('dimension')->prototype())) }} --}}
             {{-- period document link --}}
             <div class="hidden collection-container title" form_type="target_document_link"
               data-prototype="{{ str_replace('target[0][document_link][__NAME__]','target[__PARENT_NAME__][document_link][__WRAPPER_NAME__]',form_row($form->target->getChildren()[0]->getChild('document_link')->prototype())) }}">
