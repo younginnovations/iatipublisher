b866205762955f201020613b2b511107aadb656d
A4family <athapa2016@hotmail.com>
2024-03-28 15:16:09 +0545

    feat: backup affected organization xml feat: re-generated affected organization xml

--- app/Console/Commands/FixPublishedOrganizationXmlFile1393.php
+++ app/Console/Commands/FixPublishedOrganizationXmlFile1393.php
@@ -4,15 +4,22 @@ declare(strict_types=1);
 
 namespace App\Console\Commands;
 
+use App\IATI\Elements\Xml\OrganizationXmlGenerator;
 use App\IATI\Models\Organization\Organization;
 use Illuminate\Console\Command;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Arr;
 
 /**
  * Class FixPublishedOrganizationXmlFile1393.
  */
 class FixPublishedOrganizationXmlFile1393 extends Command
 {
+    public function __construct(protected OrganizationXmlGenerator $xmlGenerator)
+    {
+        parent::__construct();
+    }
+
     /**
      * The name and signature of the console command.
      *
@@ -33,21 +40,69 @@ class FixPublishedOrganizationXmlFile1393 extends Command
         without modifying the last updated date.
     ';
 
-    public function handle()
+    /**
+     * @return void
+     */
+    public function handle(): void
     {
-        $publishedOrganizations = $this->getPublishedOrganizations();
-        dd($publishedOrganizations);
+        echo 'Do you want to continue? (yes/no)';
+
+        if (strtolower(trim(fgets(STDIN))) === 'yes') {
+            $publishedOrganizations = $this->getPublishedOrganizations();
+
+            foreach ($publishedOrganizations as $organization) {
+                if (array_key_exists('recipient_country', Arr::collapse($publishedOrganizations[0]->document_link)) && !is_array_value_empty(Arr::collapse($publishedOrganizations[0]->document_link)['recipient_country'])) {
+                    $organizationXmlFile = awsGetFile("organizationXmlFiles/$organization->publisher_id-organisation.xml");
+
+                    if ($organizationXmlFile) {
+                        try {
+                            $xmlFile = simplexml_load_string($organizationXmlFile);
+                            $organization = Organization::find($organization->id);
+                            $settings = $organization->settings;
+
+                            if (!count($xmlFile->xpath('//iati-organisation/document-link/recipient-country'))) {
+                                $this->takeOrganizationXmlFileBackup($organization);
+                                $this->xmlGenerator->generateOrganizationXml($settings, $organization, false);
+                                $this->info("organizationXmlFiles/$organization->publisher_id-organisation.xml");
+                            }
+                        } catch (\Exception | \Throwable $exception) {
+                            $message = 'Organization Xml File generation failed - ' . $organization->publisher_name;
+                            logger($message);
+                            logger()->error($exception);
+                            $this->info($message);
+                        }
+                    }
+                }
+            }
+        }
     }
 
     /**
+     * Takes backup from affected organizations.
+     *
+     * @param $organization
+     *
+     * @return void
+     */
+    public function takeOrganizationXmlFileBackup($organization): void
+    {
+        $getFile = awsGetFile("organizationXmlFiles/$organization->publisher_id-organisation.xml");
+
+        if (awsUploadFile("organizationXmlBackupFiles/$organization->publisher_id-organisation.xml", $getFile)) {
+            $this->info('Successfully backed up organization xml file - ' . $organization->publisher_name);
+        }
+    }
+
+    /**
+     * Get organization whose status is either published or draft.
+     *
      * @return Collection|array
      */
     public function getPublishedOrganizations(): Collection|array
     {
         return Organization::query()
             ->whereNotNull('document_link')
-            ->where('status', 'published')
-            ->whereJsonLength('document_link->0->recipient_country', '>', 0)
+            ->whereIn('status', ['published', 'draft'])
             ->get();
     }
 }
