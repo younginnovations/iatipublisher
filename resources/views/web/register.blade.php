5a55af58038463a9cf75f86f4a6aa584f529eab2
Momik Shrestha <momik@localhost.localdomain>
2024-04-15 11:09:13 +0545

    -[x] Collect default values and autoset them

--- resources/views/web/register.blade.php
+++ resources/views/web/register.blade.php
@@ -5,5 +5,6 @@
       :country='{{json_encode($countries)}}'
       :agency='{{json_encode($registration_agencies)}}'
       :uncategorized-organisation-registration-agency='{{json_encode($uncategorizedRegistrationAgencyPrefix)}}'
+      :languages='{{json_encode($languages)}}'
   ></register-form>
 @endsection
