6d7cb150b2c50151659cf3849884f6fad5f8c818
Momik Shrestha <momik12339@gmail.com>
2024-06-19 11:56:17 +0545

    - [x] Added privacy policy link - [x] Added cronjob to clear audit logs older than 1 month.

---
+++ app/Console/Commands/ClearOlderAuditLogs.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Console\Commands;
+
+use Exception;
+use Illuminate\Console\Command;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
+
+/**
+ * @class ClearOlderAuditLogs
+ *
+ * Used in app/Console/Kernel.php. Scheduled to on the first of every month.
+ */
+class ClearOlderAuditLogs extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'command:ClearOlderAuditLogs';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Clear audit logs older than one month. See issue: https://github.com/younginnovations/iatipublisher/issues/1456';
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function handle(): void
+    {
+        DB::beginTransaction();
+
+        try {
+            $date = Carbon::now()->subMonth();
+
+            DB::table('audits')->where('created_at', '<', $date)->delete();
+
+            logger()->info('Successfully cleared audit logs older than one month.');
+
+            DB::commit();
+        } catch (Exception $e) {
+            DB::rollBack();
+
+            logger()->error('Error clearing audit logs: ' . $e->getMessage());
+        }
+    }
+}
