9d66b0908eaaf61ac0ca9cbe771cfa5319c0f18d
Momik Shrestha <momik12339@gmail.com>
2024-07-08 10:25:06 +0545

    -[x] Updated text in welcome page and join now page

--- resources/assets/js/views/web/partials/SignIn.vue
+++ resources/assets/js/views/web/partials/SignIn.vue
@@ -6,7 +6,7 @@
     <Loader v-if="isLoaderVisible"></Loader>
 
     <div class="right__container flex w-full flex-col" @keyup.enter="login">
-      <h2 class="mb-2 hidden sm:block">Sign In.</h2>
+      <h2 class="mb-2 hidden sm:block">Sign In</h2>
       <span class="text-n-40">Welcome back! Please enter your details.</span>
       <div
         v-if="
