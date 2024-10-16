e4702106429d89942f3affa5712adcb2234b4de9
Sanil Manandhar <sanilmanandhar@gmail.com>
2024-10-16 14:53:36 +0545

    Created command to generate xlsx file and store in aws

--- lang/en/public/login.php
+++ lang/en/public/login.php
@@ -2,4 +2,11 @@
 
 return [
     'login' => 'Login',
+    'test' => [
+        'test1' => 'test1 new',
+        'test2' => [
+            'test3' => 'test3 new',
+            'test4' => 'test4 new'
+        ]
+    ]
 ];
