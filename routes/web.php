20b1b2e693dce49e14620c26b22169a43b0d6f4e
Sanil Manandhar <sanilmanandhar@gmail.com>
2024-10-22 12:07:54 +0545

    Created api to return locale

--- routes/web.php
+++ routes/web.php
@@ -51,4 +51,5 @@ Route::get('/activities/activities_count_by_published_status', [ActivityControll
 Route::get('/duplicate-activity', [ActivityController::class, 'duplicateActivity'])->middleware('auth');
 Route::get('/language/{language}', [\App\Http\Controllers\Web\WebController::class, 'changeLocale'])->name('change-locale');
 Route::get('/language/{language}', [\App\Http\Controllers\Web\WebController::class, 'setLocale'])->name('set-locale');
+Route::get('/current-language', [\App\Http\Controllers\Web\WebController::class, 'getLocale'])->name('set-locale');
 Route::get('/translated-data/{folder}', [\App\Http\Controllers\Web\WebController::class, 'getTranslatedData'])->name('get-translated-data');
