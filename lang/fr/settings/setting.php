33a0f5e63745467fd08dcbc2e968c7daf9a143ba
Shamee <samibajra7@gmail.com>
2024-12-25 04:42:26 +0000

    settings tranlsation completed
    - [x] Refactored to used provide and inject
    
    - [x] refactor: relocate SettingController translations to a dedicated file
    
    - [x] Fix setting issue
    
    - [x] feat: settings fr and es added
    
    - [x] setting's request translation added
    
    - [x] Move request translation logic to validation file
    
    - [x] Publisher form request attributes removed
    
    - [x] Translation file formatted
    
    - [x] Fix hover not working

---
+++ lang/fr/settings/setting.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'settings'             => 'fr_ Settings',
+    'publishing_settings'  => 'fr_ Publishing Settings',
+    'registry_information' => 'fr_ Registry Information',
+    'publisher_id'         => 'fr_ Publisher ID',
+    'api_token'            => 'fr_ API Token',
+    'pending'              => 'fr_ Pending',
+    'verify'               => 'fr_ Verify',
+];
---
+++ lang/fr/settings/setting_controller.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'api_token_verified_successfully'        => 'fr_ API token verified successfully.',
+    'your_account_is_pending_approval'       => 'fr_ Your account is pending approval by the IATI team - someone should be in touch within two working days.',
+    'api_token_incorrect'                    => 'fr_ API token incorrect. Please enter valid API token.',
+    'publisher_does_not_exist_in_registry'   => 'fr_ Publisher does not exist in registry.',
+    'error_occurred_while_fetching_the_data' => 'fr_ Error occurred while fetching the data.',
+    'error_while_rendering_setting_page'     => 'fr_ Error while rendering setting page.',
+    'settings_fetched_successfully'          => 'fr_ Settings fetched successfully.',
+    'error_occurred_while_verify_publisher'  => 'fr_ Error occurred while verify publisher.',
+    'publisher_setting_stored_successfully'  => 'fr_ Publisher setting stored successfully.',
+    'error_occurred_while_verifying_data'    => 'fr_ Error occurred while verifying data.',
+    'publisher_id_or_api_token_incorrect'    => 'fr_ Publisher Id or API Token incorrect.',
+    'default_setting_stored_successfully'    => 'fr_ Default setting stored successfully.',
+    'error_occurred_while_storing_setting'   => 'fr_ Error occurred while storing setting.',
+    'your_api_token_is_invalid'              => 'fr_ Your API token is invalid.',
+    'setting_status_successfully_retrieved'  => 'fr_ Setting status successfully retrieved.',
+];
+
