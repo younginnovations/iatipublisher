8ac0042f750ea8875a4b6a4aaa59a4032abc51c6
Shamee <samibajra7@gmail.com>
2025-01-08 11:51:49 +0545

    Feat: Footer translation added both for landing page and admin

---
+++ lang/en/footer/footer.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'iati_publisher'                          => 'IATI Publisher',
+    'about'                                   => 'About',
+    'user_manual_v1'                          => 'User Manual v1.1',
+    'iati_standard'                           => 'IATI Standard',
+    'publishing_checklist'                    => 'Publishing Checklist',
+    'support'                                 => 'Support',
+    'privacy_policy'                          => 'Privacy Policy',
+    'part_of_the_iati_unified_platform'       => 'Part of the IATI Unified Platform',
+    'code_licensed_under_the_gnu_agpl'        => 'Code licensed under the GNU AGPL.',
+    'documentation_licensed_under_cc_by3'     => 'Documentation licensed under CC BY 3.0',
+    'any_questions_contact_support'           => 'Any questions? Contact Support',
+    'support_iati_standard_org'               => 'support@iatistandard.org',
+    'copyright_iati_2022_all_rights_reserved' => 'Copyright IATI 2022. All rights reserved.',
+    'organisation_list'                       => 'Organisation List',
+    'your_activities'                         => 'Your Activities',
+    'home'                                    => 'Home',
+    'sign_in'                                 => 'Sign In',
+    'join_now'                                => 'Join Now',
+];
---
+++ lang/fr/footer/footer.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'iati_publisher'                          => 'fr_ IATI Publisher',
+    'about'                                   => 'fr_ About',
+    'user_manual_v1'                          => 'fr_ User Manual v1.1',
+    'iati_standard'                           => 'fr_ IATI Standard',
+    'publishing_checklist'                    => 'fr_ Publishing Checklist',
+    'support'                                 => 'fr_ Support',
+    'privacy_policy'                          => 'fr_ Privacy Policy',
+    'part_of_the_iati_unified_platform'       => 'fr_ Part of the IATI Unified Platform',
+    'code_licensed_under_the_gnu_agpl'        => 'fr_ Code licensed under the GNU AGPL.',
+    'documentation_licensed_under_cc_by3'     => 'fr_ Documentation licensed under CC BY 3.0',
+    'any_questions_contact_support'           => 'fr_ Any questions? Contact Support',
+    'support_iati_standard_org'               => 'fr_ support@iatistandard.org',
+    'copyright_iati_2022_all_rights_reserved' => 'fr_ Copyright IATI 2022. All rights reserved.',
+    'organisation_list'                       => 'fr_ Organisation List',
+    'your_activities'                         => 'fr_ Your Activities',
+    'home'                                    => 'fr_ Home',
+    'sign_in'                                 => 'fr_ Sign In',
+    'join_now'                                => 'fr_ Join Now',
+];
