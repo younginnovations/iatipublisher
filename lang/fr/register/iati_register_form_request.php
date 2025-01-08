8476240683b89ede047fd3c2a80590afc048ce75
PG-Momik <momik12339@gmail.com>
2025-01-08 10:04:29 +0545

    Fix validation message issue on iati register form request

--- lang/fr/register/iati_register_form_request.php
+++ lang/fr/register/iati_register_form_request.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'the_publisher_id_is_invalid.'              => 'fr_ The publisher id is invalid. The publisher id must be at least two characters long and lower case. It can include letters, numbers and also - (dash) and _ (underscore).',
+    'the_publisher_id_is_invalid'               => 'fr_ The publisher id is invalid. The publisher id must be at least two characters long and lower case. It can include letters, numbers and also - (dash) and _ (underscore).',
     'the_registration_number_is_invalid'        => 'fr_ The registration number is invalid. Valid registration number includes letter, number, . and _, - (dash).',
     'the_username_is_invalid'                   => 'fr_ The username is invalid. Username must be purely lowercase alphabets followed by alphanumeric(ascii) characters and these symbols:-_',
     'email_is_already_in_use_in_iati_publisher' => 'fr_ Email is already in use in IATI Publisher.',
