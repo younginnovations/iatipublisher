f0185df1ff3ef7fc63bae0855212e9ad5afad6dd
shameebajra <bajrasami7@gmail.com>
2025-02-20 14:32:12 +0545

    missed translation key fixed

--- resources/assets/js/views/web/IatiRegisterPage.vue
+++ resources/assets/js/views/web/IatiRegisterPage.vue
@@ -722,10 +722,7 @@ export default defineComponent({
                 'public.register.not_registered_page.register_section.source'
               ],
             name: 'source',
-            placeholder:
-              props.translatedData[
-                'public.register.not_registered_page.register_section.source_placeholder'
-              ],
+            placeholder: props.translatedData['common.common.select_an_option'],
             id: 'contact-email',
             required: true,
             hover_text:
